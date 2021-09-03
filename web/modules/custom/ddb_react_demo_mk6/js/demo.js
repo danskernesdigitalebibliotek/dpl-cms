99a75362a7ebf5b1085e150edcee98b09ef27cb7
Mikkel Jakobsen <mikkel@reload.dk>
2021-09-03 13:57:26 +0200

    Correcting formatting. Mostly adding newlines in end of files.
    Issue:
    https://reload.atlassian.com/browse/DDFDPDEL-85

--- web/modules/custom/ddb_react_demo_mk6/js/demo.js
+++ web/modules/custom/ddb_react_demo_mk6/js/demo.js
@@ -1,49 +1,36 @@
-  
-//   (function ($, Drupal) {
-//     Drupal.behaviors.myModuleBehavior = {
-//       attach: function (context, settings) {
-//         console.log('pikspiller');
+(function (Drupal) {
+  "use strict";
 
-//       }
-//     };
-//   })(jQuery, Drupal);
-  
-
-  (function (Drupal) {
-    "use strict";
-
-    // Behaviors might be called with a DOM element (document on page
-    // load) or a jQuery object (on AJAX load). DDB React expects a DOM
-    // element with querySelectorAll, so this tries to do the right
-    // thing.
-    var getElement = function(element) {
-      if (typeof element.querySelectorAll !== 'function') {
-        element = element[0] || element;
-      }
-  
-      if (typeof element.querySelectorAll !== 'function') {
-        return null;
-      }
-  
-      return element;
+  // Behaviors might be called with a DOM element (document on page
+  // load) or a jQuery object (on AJAX load). DDB React expects a DOM
+  // element with querySelectorAll, so this tries to do the right
+  // thing.
+  var getElement = function(element) {
+    if (typeof element.querySelectorAll !== 'function') {
+      element = element[0] || element;
     }
-  
-    Drupal.behaviors.myModuleBehavior = {
-        attach: function (context, settings) {
-          console.log('hello');
-          window.ddbReact.mount(context);
-            // Ensure that we have a DOM element.
-            var element = getElement(context);
-            window.ddbReact.mount(element);
 
-        },
-        detach: function(context) {
-            var element = getElement(context);
-            if (element) {
-              window.ddbReact.unmount(element);
-            }
+    if (typeof element.querySelectorAll !== 'function') {
+      return null;
+    }
+
+    return element;
+  }
+
+  Drupal.behaviors.myModuleBehavior = {
+      attach: function (context, settings) {
+        window.ddbReact.mount(context);
+          // Ensure that we have a DOM element.
+          var element = getElement(context);
+          window.ddbReact.mount(element);
+
+      },
+      detach: function(context) {
+          var element = getElement(context);
+          if (element) {
+            window.ddbReact.unmount(element);
           }
-      
-      };
+        }
+    
+    };
   }(Drupal));
-  
--- web/modules/custom/ddb_react_demo_mk6/src/Controller/DddbReactDemoController.php
+++ web/modules/custom/ddb_react_demo_mk6/src/Controller/DddbReactDemoController.php
@@ -1,26 +1,33 @@
 <?php
+
 namespace Drupal\ddb_react_demo_mk6\Controller;
+
 use Drupal\Core\Controller\ControllerBase;
 
-class DddbReactDemoController extends ControllerBase
-{
-    public function index()
-    {
-        $checklist_data = [
-            'material-list-url' => ding_react_material_list_url(),
-            'cover-service-url' => ding_react_cover_service_url(),
-            // We cannot use url() here as it will encode the colon in the placeholder.
-            'material-url' => '/ting/object/:pid',
-            'author-url' => '/search/ting/phrase.creator=":author"',
-            'remove-button-text' => 'Remove from list',
-            'empty-list-text' => 'List is empty.',
-            'error-text' => 'An error occurred while trying to fetch list.',
-            'of-text' => 'by'
-          ];
+/**
+ * This is used for demoing the react components.
+ */
+class DddbReactDemoController extends ControllerBase {
 
-        return [
-            'checklist' => ding_react_app('checklist', $checklist_data),
-            'button' => ding_react_ding_entity_buttons(),
-        ];
-    }
+  /**
+   * Render react components.
+   */
+  public function index() {
+    $checklist_data = [
+      'material-list-url' => ding_react_material_list_url(),
+      'cover-service-url' => ding_react_cover_service_url(),
+      'material-url' => '/ting/object/:pid',
+      'author-url' => '/search/ting/phrase.creator=":author"',
+      'remove-button-text' => 'Remove from list',
+      'empty-list-text' => 'List is empty.',
+      'error-text' => 'An error occurred while trying to fetch list.',
+      'of-text' => 'by',
+    ];
+
+    return [
+      'checklist' => ding_react_app('checklist', $checklist_data),
+      'button' => ddb_react_demo_mk6_button(),
+    ];
+  }
+
 }
