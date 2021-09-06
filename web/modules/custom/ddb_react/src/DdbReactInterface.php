157eb81bce63f23ccb6ed0eff8f649463a386135
Mikkel Jakobsen <mikkel@reload.dk>
2021-09-06 12:04:57 +0200

    Splitting integration and demo rendering into two modules.
    Issue:
    https://reload.atlassian.com/browse/DDFDPDEL-85

---
+++ web/modules/custom/ddb_react/src/DdbReactInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\ddb_react;
+
+/**
+ * Interface for ddb_react constants.
+ */
+interface DdbReactInterface {
+
+  const DDB_REACT_FOLLOW_SEARCHES_URL = 'https://prod.followsearches.dandigbib.org';
+  const DDB_REACT_MATERIAL_LIST_URL = 'https://prod.materiallist.dandigbib.org';
+  const DDB_REACT_COVER_SERVICE_URL = 'https://cover.dandigbib.org/api/v2';
+
+}
