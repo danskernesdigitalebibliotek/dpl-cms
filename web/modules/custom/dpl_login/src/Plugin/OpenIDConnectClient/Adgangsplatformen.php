a3c03df75dd6da1665cd52004a2122426fd02f8f
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2021-10-16 00:09:31 +0200

    Feature/ddfdpdel 110 initial setup openid connect dpl login (#12)
    * Adding dpl_login module for handling the oauth2 authorization/authentication flow against the external service: Adgangsplatformen.
    It is the initial implementation that proves the complete flow is working.
    
    * Make sure that the adgangsplatform plugin is enabled and default openid_connect user settings are set.

---
+++ web/modules/custom/dpl_login/src/Plugin/OpenIDConnectClient/Adgangsplatformen.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\dpl_login\Plugin\OpenIDConnectClient;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\openid_connect\Plugin\OpenIDConnectClientBase;
+use Drupal\Core\GeneratedUrl;
+
+/**
+ * Adgangsplatformen openid_connect plugin.
+ *
+ * @OpenIDConnectClient(
+ *   id = "adgangsplatformen",
+ *   label = @Translation("Adgangsplatformen")
+ * )
+ */
+class Adgangsplatformen extends OpenIDConnectClientBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'authorization_endpoint' => 'https://login.bib.dk/oauth/authorize',
+      'token_endpoint' => 'https://login.bib.dk/oauth/token/',
+      'userinfo_endpoint' => 'https://login.bib.dk/userinfo/',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['authorization_endpoint'] = [
+      '#title' => $this->t('Authorization endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['authorization_endpoint'],
+    ];
+    $form['token_endpoint'] = [
+      '#title' => $this->t('Token endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['token_endpoint'],
+    ];
+    $form['userinfo_endpoint'] = [
+      '#title' => $this->t('UserInfo endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_endpoint'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEndpoints() {
+    return [
+      'authorization' => $this->configuration['authorization_endpoint'],
+      'token' => $this->configuration['token_endpoint'],
+      'userinfo' => $this->configuration['userinfo_endpoint'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getUrlOptions($scope, GeneratedUrl $redirect_uri) {
+    $options = parent::getUrlOptions($scope, $redirect_uri);
+
+    return [
+      'query' => [
+        // TODO: Should not be hardcoded.
+        // We find another way. Probably through a config variable.
+        'agency' => '710100',
+      ] + $options['query'],
+    ] + $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Adgangsplatformen doesn't return an ID Token. The inherited method cannot
+   * handle / decode the missing / null value so we hardcode the decoded value
+   * to be an empty array.
+   */
+  public function decodeIdToken($id_token) {
+    return [];
+  }
+
+}
---
+++ web/modules/custom/dpl_login/src/Routing/RouteSubscriber.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\dpl_login\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // The oauth client setting is validating the redirect url
+    // and expect it to be: /adgangsplatformen/callback.
+    // We rewire the routing and point the route
+    // to the callback of openid_connect.
+    // And send the plugin id along the way (client_name)
+    // so openid_connect kbnows which plugin to use.
+    //
+    // @see Drupal\dpl_login\Plugin\OpenIDConnectClient\Adgangsplatformen.
+    if ($route = $collection->get('openid_connect.redirect_controller_redirect')) {
+      $route->setPath('/adgangsplatformen/callback');
+      $route->setDefault('client_name', 'adgangsplatformen');
+    }
+  }
+
+}
