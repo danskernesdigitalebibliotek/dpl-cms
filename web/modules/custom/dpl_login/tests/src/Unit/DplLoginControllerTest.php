4d30b5405a9f0e50f45110280c58d9856cccbc97
Mikkel Jakobsen <mikkel@reload.dk>
2021-11-01 11:19:38 +0100

    PR review changes:
    No need for 'access content' permission check
    
    Add a TODO with a suggestion to centralize adgangsplatform configuration handling
    
    Removing killswitch. It is not needed because of the 'no_cache' parameter in the route
    
    Another TODO about centralizing adgangsplatform conf
    
    Make sure that user is still logged out although an access token was missing
    
    Handling logout for users that is not authenticated through Adgangsplatformen
    
    Adding TODO about possible future mock implementation of user_logout
    
    Adding assertions for responses in logout tests and test for non-adgangsplatform users

--- web/modules/custom/dpl_login/tests/src/Unit/DplLoginControllerTest.php
+++ web/modules/custom/dpl_login/tests/src/Unit/DplLoginControllerTest.php
@@ -4,22 +4,24 @@ namespace Drupal\Tests\dpl_library_token\Unit;
 
 use Prophecy\Argument;
 use Psr\Log\LoggerInterface;
+use Drupal\Core\GeneratedUrl;
 use Drupal\Tests\UnitTestCase;
+use Drupal\dpl_login\AccessToken;
+use Drupal\Core\Routing\UrlGenerator;
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\dpl_login\UserTokensProvider;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\dpl_library_token\LibraryTokenHandler;
 use Drupal\dpl_login\Controller\DplLoginController;
-use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
-use Drupal\Core\GeneratedUrl;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Routing\UrlGenerator;
 use Drupal\Core\Utility\UnroutedUrlAssemblerInterface;
-use Drupal\dpl_login\AccessToken;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\dpl_login\Exception\MissingConfigurationException;
 
 // Mocking user_logout function.
+// TODO: Could be solved with php-mock in the future.
 require_once 'user_logout.mock.php';
 
 /**
@@ -35,8 +37,6 @@ class DplLoginControllerTest extends UnitTestCase {
     $logger->error(Argument::any(), Argument::any())->shouldNotBeCalled();
     $logger_factory = $this->prophesize(LoggerChannelFactoryInterface::class);
     $logger_factory->get(Argument::any())->willReturn($logger->reveal());
-
-    $kill_switch = $this->prophesize(KillSwitch::class);
 
     $fake_access_token = AccessToken::createFromOpenidConnectContext([
       'tokens' => [
@@ -60,7 +60,6 @@ class DplLoginControllerTest extends UnitTestCase {
 
     $container = new ContainerBuilder();
     $container->set('logger.factory', $logger_factory->reveal());
-    $container->set('page_cache_kill_switch', $kill_switch->reveal());
     $container->set('dpl_login.user_tokens', $user_token_provider->reveal());
     $container->set('config.factory', $config_factory->reveal());
     $container->set('unrouted_url_assembler', $unrouted_url_assembler->reveal());
@@ -81,9 +80,9 @@ class DplLoginControllerTest extends UnitTestCase {
   }
 
   /**
-   * Make sure an config missing exception is NOT thrown.
+   * The user is redirected to external login if everything is ok.
    */
-  public function testThatExceptionIsNotThrownIfLogoutEndpointIsPresent(): void {
+  public function testThatExternalRedirectIsActivatedIfEverythingIsOk(): void {
     $config = $this->prophesize(ImmutableConfig::class);
     $config->get('settings')->willReturn([
       'logout_endpoint' => 'https://valid.uri',
@@ -96,7 +95,44 @@ class DplLoginControllerTest extends UnitTestCase {
     \Drupal::setContainer($container);
 
     $controller = DplLoginController::create($container);
-    $controller->logout();
+    $response = $controller->logout();
+
+    $this->assertInstanceOf(TrustedRedirectResponse::class, $response);
+    $this->assertSame(
+      'https://valid.uri?singlelogout=true&access_token=dasjhsadhadsjkhkajsdhkj&redirect_uri=https%3A//local.site',
+      $response->headers->get('location')
+    );
+  }
+
+  /**
+   * Test that normal Drupal users (admins get logged out.
+   */
+  public function testThatAdminsGetLoggedOut(): void {
+    $config = $this->prophesize(ImmutableConfig::class);
+    $config->get('settings')->willReturn([
+      'logout_endpoint' => 'https://valid.uri',
+    ])->shouldBeCalledTimes(1);
+    $config_factory = $this->prophesize(ConfigFactoryInterface::class);
+    $config_factory->get(LibraryTokenHandler::SETTINGS_KEY)->willReturn($config->reveal());
+    $user_token_provider = $this->prophesize(UserTokensProvider::class);
+    $user_token_provider->getAccessToken()->willReturn(NULL);
+    $url_generator = $this->prophesize(UrlGenerator::class);
+    $url_generator->generateFromRoute('<front>', Argument::cetera())->willReturn('https://local.site');
+
+    $container = \Drupal::getContainer();
+    $container->set('config.factory', $config_factory->reveal());
+    $container->set('dpl_login.user_tokens', $user_token_provider->reveal());
+    $container->set('url_generator', $url_generator->reveal());
+    \Drupal::setContainer($container);
+
+    $controller = DplLoginController::create($container);
+    $response = $response = $controller->logout();
+
+    $this->assertInstanceOf(RedirectResponse::class, $response);
+    $this->assertSame(
+      'https://local.site',
+      $response->headers->get('location')
+    );
   }
 
 }
