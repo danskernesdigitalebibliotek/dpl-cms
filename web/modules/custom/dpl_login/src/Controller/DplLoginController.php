5410d23741f9b981b5dfdf6b75d5fa9c47274f6c
Mikkel Jakobsen <mikkel@reload.dk>
2021-11-01 11:19:38 +0100

    PR review changes:
    No need for 'access content' permission check
    
    Add a TODO with a suggestion to centralize adgangsplatform configuration handling
    
    Removing killswitch. It is not needed because of the 'no_cache' parameter in the route
    
    Another TODO about centralizing adgangsplatform conf
    
    Make sure that user is still logged out although an access token was missing
    
    Handling logout for users that is not authenticated through Adgangsplatformen
    
    Adding TODO about possible future mock implementation of user_logout
    
    Adding assertions for responses in logout tests and test for non-adgangsplatform users

--- web/modules/custom/dpl_login/src/Controller/DplLoginController.php
+++ web/modules/custom/dpl_login/src/Controller/DplLoginController.php
@@ -3,13 +3,10 @@
 namespace Drupal\dpl_login\Controller;
 
 use Drupal\Core\Url;
-use Psr\Log\LogLevel;
 use Drupal\dpl_login\UserTokensProvider;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Routing\TrustedRedirectResponse;
-use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\dpl_login\Exception\MissingConfigurationException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -21,6 +18,9 @@ class DplLoginController extends ControllerBase {
   use StringTranslationTrait;
 
   const LOGGER_KEY = 'dpl_login';
+  // TODO: This could be moved to a new service
+  // handling adgangsplatform configuration.
+  // @see dpl_login_install() and \Drupal\dpl_library_token\LibraryTokenHandler.
   const SETTINGS_KEY = 'openid_connect.settings.adgangsplatformen';
 
   /**
@@ -29,18 +29,6 @@ class DplLoginController extends ControllerBase {
    * @var \Drupal\dpl_login\UserTokensProvider
    */
   protected userTokensProvider $userTokensProvider;
-  /**
-   * The page cache kill switch service.
-   *
-   * @var \Drupal\Core\PageCache\ResponsePolicy\KillSwitch
-   */
-  protected KillSwitch $killSwitch;
-  /**
-   * The logger.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelInterface
-   */
-  protected $logger;
   /**
    * The Messenger service.
    *
@@ -59,22 +47,14 @@ class DplLoginController extends ControllerBase {
    *
    * @param \Drupal\dpl_login\UserTokensProvider $user_token_provider
    *   The Uuser token provider.
-   * @param \Drupal\Core\PageCache\ResponsePolicy\KillSwitch $killSwitch
-   *   The page cache kill switch service.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
-   *   The DPL login logger channel.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    *   Configuration.
    */
   public function __construct(
     UserTokensProvider $user_token_provider,
-    KillSwitch $killSwitch,
-    LoggerChannelFactoryInterface $logger,
     ConfigFactoryInterface $configFactory
   ) {
     $this->userTokensProvider = $user_token_provider;
-    $this->killSwitch = $killSwitch;
-    $this->logger = $logger->get(self::LOGGER_KEY);
     $this->settings = $configFactory
       ->get(self::SETTINGS_KEY)->get('settings');
   }
@@ -90,9 +70,7 @@ class DplLoginController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('dpl_login.user_tokens'),
-      $container->get('page_cache_kill_switch'),
-      $container->get('logger.factory'),
-      $container->get('config.factory'),
+      $container->get('config.factory')
     );
   }
 
@@ -105,25 +83,27 @@ class DplLoginController extends ControllerBase {
    *   Redirect to external logout service or front if not possible.
    */
   public function logout() {
-    // We need this to prevent the redircet to be cached.
-    $this->killSwitch->trigger();
-
     // It is a global problem if the logout endpoint has not been configured.
+    // TODO: This could be moved to a new service
+    // handling adgangsplatform configuration.
+    // @see dpl_login_install() and \Drupal\dpl_library_token\LibraryTokenHandler.
     if (!$logout_endpoint = $this->settings['logout_endpoint'] ?? NULL) {
       throw new MissingConfigurationException('Adgangsplatformen plugin config variable logout_endpoint is missing');
     }
 
-    // Handle case of missing access token.
-    if (!$access_token = $this->userTokensProvider->getAccessToken()) {
-      $this->logger
-        ->log(LogLevel::ERROR, 'Cannot logut user remotely because of missing access token');
-      return $this->redirect('<front>');
-    }
+    $access_token = $this->userTokensProvider->getAccessToken();
 
     // Log out user in Drupal.
     // We do this regardless wether it is possible to logout remotely or not.
     // We do not want the user to get stuck on the site in a logged in state.
     user_logout();
+
+    // Handle case of a user that is either:
+    // NOT authenticated by Adgangsplatformen
+    // or is missing its access token.
+    if (!$access_token) {
+      return $this->redirect('<front>');
+    }
 
     // Create url for logout service that it should redirect back to.
     // Since toString(TRUE) is called
