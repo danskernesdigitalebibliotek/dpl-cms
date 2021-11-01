d820d5ad7c56806bd5503f0d7a3bf297201caf4a
Mikkel Jakobsen <mikkel@reload.dk>
2021-11-01 15:18:21 +0100

    Adding dpl roles and attach role to new users.

---
+++ web/modules/custom/dpl_login/src/DplLoginInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\dpl_login;
+
+/**
+ * Interface for dpl_login constants.
+ */
+interface DplLoginInterface {
+
+  const DPL_ROLES_AUTHENTICATED_REGISTERED_READER = 'authenticated_registered_reader';
+
+}
