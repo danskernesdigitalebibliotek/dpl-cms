468c532b7f4ba094ff036bd30be8b6d43ba7c2b5
Mads H. Danquah <mads@danquah.dk>
2021-11-04 08:01:07 +0100

    Add all.settings.php to phpcs and reformat the file

--- assets/all.settings.php
+++ assets/all.settings.php
@@ -1,77 +1,82 @@
 <?php
 
-use Drupal\Core\Installer\InstallerKernel;
-
 /**
  * @file
- * amazee.io Drupal all environment configuration file.
+ * Amazee.io Drupal all environment configuration file.
  *
- * This file should contain all settings.php configurations that are needed by all environments.
+ * This file should contain all settings.php configurations that are needed by
+ * all environments.
  *
- * It contains some defaults that the amazee.io team suggests, please edit them as required.
+ * It contains some defaults that the amazee.io team suggests, please edit them
+ * as required.
  */
+
+use Drupal\Core\Installer\InstallerKernel;
 
 // Hardcode a Site UUID to enable sharing of configuration between sites beyond
 // site install.
 $config['system.site']['uuid'] = '13ef1a53-dfb4-4c82-9b64-44586a366729';
 
-// Defines where the sync folder of your configuration lives. In this case it's inside
-// the Drupal root, which is protected by amazee.io Nginx configs, so it cannot be read
-// via the browser. If your Drupal root is inside a subfolder (like 'web') you can put the config
-// folder outside this subfolder for an advanced security measure: '../config/sync'.
+// Defines where the sync folder of your configuration lives. In this case it's
+// inside the Drupal root, which is protected by amazee.io Nginx configs, so it
+// cannot be read via the browser. If your Drupal root is inside a subfolder
+// (like 'web') you can put the config folder outside this subfolder for an
+// advanced security measure: '../config/sync'.
 $settings['config_sync_directory'] = '../config/sync';
 
 if (getenv('LAGOON_ENVIRONMENT_TYPE') !== 'production') {
-    /**
-     * Skip file system permissions hardening.
-     *
-     * The system module will periodically check the permissions of your site's
-     * site directory to ensure that it is not writable by the website user. For
-     * sites that are managed with a version control system, this can cause problems
-     * when files in that directory such as settings.php are updated, because the
-     * user pulling in the changes won't have permissions to modify files in the
-     * directory.
-     */
-    $settings['skip_permissions_hardening'] = TRUE;
+  // Skip file system permissions hardening.
+  //
+  // The system module will periodically check the permissions of your site's
+  // site directory to ensure that it is not writable by the website user. For
+  // sites that are managed with a version control system, this can cause
+  // problems when files in that directory such as settings.php are updated,
+  // because the user pulling in the changes won't have permissions to modify
+  // files in the directory.
+  $settings['skip_permissions_hardening'] = TRUE;
 }
 
 // Setup Redis.
 if (getenv('LAGOON')) {
-    $settings['redis.connection']['interface'] = 'PhpRedis';
-    $settings['redis.connection']['host'] = getenv('REDIS_HOST') ?: 'redis';
-    $settings['redis.connection']['port'] = getenv('REDIS_SERVICE_PORT') ?: '6379';
-    $settings['cache_prefix']['default'] = getenv('LAGOON_PROJECT') . '_' . getenv('LAGOON_GIT_SAFE_BRANCH');
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+  $settings['redis.connection']['host'] = getenv('REDIS_HOST') ?: 'redis';
+  $settings['redis.connection']['port'] = getenv('REDIS_SERVICE_PORT') ?: '6379';
+  $settings['cache_prefix']['default'] = getenv('LAGOON_PROJECT') . '_' . getenv('LAGOON_GIT_SAFE_BRANCH');
 
-    // Do not set the cache during installations of Drupal.
-    if (!InstallerKernel::installationAttempted() && extension_loaded('redis')) {
-      $settings['cache']['default'] = 'cache.backend.redis';
+  // Do not set the cache during installations of Drupal.
+  if (!InstallerKernel::installationAttempted() && extension_loaded('redis')) {
+    $settings['cache']['default'] = 'cache.backend.redis';
 
-      // And allows to use it without the Redis module being enabled.
-      $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+    // And allows to use it without the Redis module being enabled.
+    $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
 
-      $settings['bootstrap_container_definition'] = [
-        'parameters' => [],
-        'services' => [
-          'redis.factory' => [
-            'class' => 'Drupal\redis\ClientFactory',
-          ],
-          'cache.backend.redis' => [
+    $settings['bootstrap_container_definition'] = [
+      'parameters' => [],
+      'services' => [
+        'redis.factory' => [
+          'class' => 'Drupal\redis\ClientFactory',
+        ],
+        'cache.backend.redis' => [
           'class' => 'Drupal\redis\Cache\CacheBackendFactory',
-            'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
-          ],
-          'cache.container' => [
-          'class' => '\Drupal\redis\Cache\PhpRedis',
-          'factory' => ['@cache.backend.redis', 'get'],
-            'arguments' => ['container'],
-          ],
-          'cache_tags_provider.container' => [
-          'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
-            'arguments' => ['@redis.factory'],
-          ],
-          'serialization.phpserialize' => [
-            'class' => 'Drupal\Component\Serialization\PhpSerialize',
+          'arguments' => [
+            '@redis.factory',
+            '@cache_tags_provider.container',
+            '@serialization.phpserialize',
           ],
         ],
-      ];
-    }
+        'cache.container' => [
+          'class' => '\Drupal\redis\Cache\PhpRedis',
+          'factory' => ['@cache.backend.redis', 'get'],
+          'arguments' => ['container'],
+        ],
+        'cache_tags_provider.container' => [
+          'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+          'arguments' => ['@redis.factory'],
+        ],
+        'serialization.phpserialize' => [
+          'class' => 'Drupal\Component\Serialization\PhpSerialize',
+        ],
+      ],
+    ];
   }
+}
