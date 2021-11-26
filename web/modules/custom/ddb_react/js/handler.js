c8f7fa8df2539fe5d1cfa7b845c909145f338c74
Kasper Garnaes <kasper.garnaes@gmail.com>
2021-11-26 15:41:05 +0100

    Fix Eslint errors in JavaScript code
    This is the result of running eslint --fix and then addressing the
    remaining issues manually.
    
    The eslint configuration has intentionally been updated to allow
    console.debug/warn/error. Console logging has its use in current 
    frontend development and these functions indicate that the developer
    has taken the context of what is being logged and why into 
    consideration.

--- web/modules/custom/ddb_react/js/handler.js
+++ web/modules/custom/ddb_react/js/handler.js
@@ -1,40 +1,39 @@
-(function (Drupal, $) {
-  "use strict";
+(function ddbReact(Drupal, $) {
   // Behaviors might be called with a DOM element (document on page
   // load) or a jQuery object (on AJAX load). DDB React expects a DOM
   // element with querySelectorAll, so this tries to do the right
   // thing.
-  var getElement = function(element) {
-    if (typeof element.querySelectorAll !== 'function') {
+  const getElement = function getElement(element) {
+    if (typeof element.querySelectorAll !== "function") {
       element = element[0] || element;
     }
 
-    if (typeof element.querySelectorAll !== 'function') {
+    if (typeof element.querySelectorAll !== "function") {
       return null;
     }
 
     return element;
-  }
+  };
 
   Drupal.behaviors.ddbReactHandler = {
-      attach: function (context, settings) {
-          var element = getElement(context);
-          if (element) {
-            $.ajax({
-              url: '/ddb-react/user.js',
-              dataType: 'script',
-              cache: true,
-              success: function () {
-                window.ddbReact.mount(element);
-              }
-            });
+    attach(context) {
+      const element = getElement(context);
+      if (element) {
+        $.ajax({
+          url: "/ddb-react/user.js",
+          dataType: "script",
+          cache: true,
+          success() {
+            window.ddbReact.mount(element);
           }
-      },
-      detach: function(context) {
-          var element = getElement(context);
-          if (element) {
-            window.ddbReact.unmount(element);
-          }
-        }
-    };
-  }(Drupal, jQuery));
+        });
+      }
+    },
+    detach(context) {
+      const element = getElement(context);
+      if (element) {
+        window.ddbReact.unmount(element);
+      }
+    }
+  };
+})(Drupal, jQuery);
--- web/modules/custom/dpl_mapp/js/dpl_mapp.js
+++ web/modules/custom/dpl_mapp/js/dpl_mapp.js
@@ -2,36 +2,40 @@
  * @file
  * Handles Mapp tracking in the browser.
  */
+/* global wts:false */
 
-(function(once, drupalSettings) {
-  "use strict";
-
-  var pushEvent = function pushEvent(eventId, eventData) {
-    console.debug('DPL Mapp: Pushing %s event %o', eventId, eventData);
+(function dplMapp(once, wts) {
+  const pushEvent = function pushEvent(eventId, eventData) {
+    console.debug("DPL Mapp: Pushing %s event %o", eventId, eventData);
 
     // Ensure that the Mapp object is defined before pushing event.
-    if (typeof wts !== 'undefined') {
-      wts.push(['send', eventId, eventData]);
+    if (typeof wts !== "undefined") {
+      wts.push(["send", eventId, eventData]);
     }
   };
 
   Drupal.behaviors.dpl_mapp = {
-    attach: function (context) {
+    attach(context) {
       // Send Mapp events attached through the HTML.
-      once('js-dpl-mapp', '.js-dpl-mapp', context).forEach(function (el) {
-        el.addEventListener('click', function(el) {
-          const eventId = this.dataset.dplMappEventId ?? 'click';
-          const eventDataString = this.dataset.dplMappEventData;
-          let eventData = {};
-          try {
-            eventData = JSON.parse(eventDataString);
-          } catch (e) {
-            console.debug('DPL Mapp: Event data not recognized as JSON: %s', eventDataString)
-          }
+      once("js-dpl-mapp", ".js-dpl-mapp", context).forEach(
+        function elementHandler(el) {
+          el.addEventListener("click", function clickHandler() {
+            const eventId = this.dataset.dplMappEventId || "click";
+            const eventDataString = this.dataset.dplMappEventData;
+            let eventData = {};
+            try {
+              eventData = JSON.parse(eventDataString);
+            } catch (e) {
+              console.debug(
+                "DPL Mapp: Event data not recognized as JSON: %s",
+                eventDataString
+              );
+            }
 
-          pushEvent(eventId, eventData);
-        });
-      });
+            pushEvent(eventId, eventData);
+          });
+        }
+      );
     }
-  }
-})(once, drupalSettings);
+  };
+})(once, wts);
