dafc70981d2090e94cfdf5323e62b403420e18fc
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2021-12-02 14:30:46 +0100

    In future we can dictate which redirect url should be used in connection with an oauth grant
    So there is no reason to use the old /adgangsplatformen/callback.
    And we lean on what openid_connect expects (/openid-connect/adgansplatformen).
    
    Issue:
    https://reload.atlassian.net/browse/DDFDPDEL-197

--- web/modules/custom/dpl_login/src/Routing/RouteSubscriber.php
+++ web/modules/custom/dpl_login/src/Routing/RouteSubscriber.php
@@ -14,19 +14,6 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection): void {
-    // The oauth client setting is validating the redirect url
-    // and expect it to be: /adgangsplatformen/callback.
-    // We rewire the routing and point the route
-    // to the callback of openid_connect.
-    // And send the plugin id along the way (client_name)
-    // so openid_connect kbnows which plugin to use.
-    //
-    // @see Drupal\dpl_login\Plugin\OpenIDConnectClient\Adgangsplatformen.
-    if ($route = $collection->get('openid_connect.redirect_controller_redirect')) {
-      $route->setPath('/adgangsplatformen/callback');
-      $route->setDefault('client_name', 'adgangsplatformen');
-    }
-
     // Rewire the core logout route to ours
     // so we can logout users remotely as well.
     if ($route = $collection->get('user.logout')) {
