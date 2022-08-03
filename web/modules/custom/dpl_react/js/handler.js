f27edb5b44f59c3c8d6a3cd40ff90cc3972d6690
Kasper Garnaes <kasper.garnaes@gmail.com>
2022-08-03 11:13:02 +0200

    Remove dependency on jQuery from React handler
    We only use it to retrieve access tokens. This can be replaced with
    vanilla JavaScript and Fetch API. 
    https://github.com/jquery/jquery/blob/3.6.0/src/ajax/script.js
    
    This is currently our only dependency on jQuery. Removing it should
    shave off a considerable amount of JavaScript that clients no longer
    have to parse. This improves frontend performance.

--- web/modules/custom/dpl_react/js/handler.js
+++ web/modules/custom/dpl_react/js/handler.js
@@ -1,4 +1,4 @@
-(function dplReact(Drupal, $) {
+(function dplReact(Drupal) {
   // Behaviors might be called with a DOM element (document on page
   // load) or a jQuery object (on AJAX load). DPL React expects a DOM
   // element with querySelectorAll, so this tries to do the right
@@ -15,18 +15,39 @@
     return element;
   };
 
+  // Simplified port of jQuery DOMEval to vanilla JS.
+  // https://github.com/jquery/jquery/blob/3.6.0/src/core/DOMEval.js
+  const DOMEval = function DOMEval(code) {
+    const doc = document;
+    const script = doc.createElement("script");
+    script.text = code;
+    doc.head.appendChild(script).parentNode.removeChild(script);
+  };
+
   Drupal.behaviors.dplReactHandler = {
     attach(context) {
       const element = getElement(context);
       if (element) {
-        $.ajax({
-          url: "/dpl-react/user-tokens",
-          dataType: "script",
-          cache: true,
-          success() {
-            window.dplReact.mount(element);
+        // Port jQuery.ajax with dataType script to vanilla JS with Fetch API.
+        fetch("/dpl-react/user-tokens", {
+          headers: {
+            Accept:
+              "text/javascript, application/javascript, " +
+              "application/ecmascript, application/x-ecmascript"
           }
-        });
+        })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(
+                `HTTP error response: ${response.status} - ${response.statusText}`
+              );
+            }
+            return response.text();
+          })
+          .then(response => {
+            DOMEval(response);
+            window.dplReact.mount(element);
+          });
       }
     },
     detach(context) {
@@ -36,4 +57,4 @@
       }
     }
   };
-})(Drupal, jQuery);
+})(Drupal);
