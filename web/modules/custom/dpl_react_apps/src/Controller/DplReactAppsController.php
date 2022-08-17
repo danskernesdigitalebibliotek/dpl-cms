c5256034fc925eed78fe2258dbeca7078b9e6c88
Kasper Garnaes <kasper.garnaes@gmail.com>
2022-08-17 23:05:53 +0200

    Add all known props to the work/material app
    The makes the app run and uses the translation features by Drupal.

--- web/modules/custom/dpl_react_apps/src/Controller/DplReactAppsController.php
+++ web/modules/custom/dpl_react_apps/src/Controller/DplReactAppsController.php
@@ -51,20 +51,48 @@ class DplReactAppsController extends ControllerBase {
    *   Render array.
    */
   public function work(string $wid): array {
+    $search_result_url = Url::fromRoute('dpl_react_apps.search_result')->toString();
+
     // Translation context.
     $c = ['context' => 'Work Page'];
 
     return [
       'material' => dpl_react_render('material', [
         'wid' => $wid,
+        'search-url' => $search_result_url,
+        // TODO Consider if we can get this value from the routing instead of
+        // hardcoding it.
+        'material-url' => '/work/:workid',
         'material-header-author-by-text' => $this->t('By', [], $c),
         'periodikum-select-year-text' => $this->t('Year', [], $c),
         'periodikum-select-week-text' => $this->t('Week', [], $c),
         'reserve-book-text' => $this->t('Reserve book', [], $c),
-        'fine-on-bookshelf-text' => $this->t('Find at book Shelf', [], $c),
-        'in-the-same-series-text' => $this->t('In the same series', [], $c),
-        'subjects-text' => $this->t('Subjects', [], $c),
-        'number-in-series-text' => $this->t('in series', [], $c),
+        'find-on-bookshelf-text' => $this->t('Find on book shelf', [], $c),
+        'description-headline-text' => $this->t('Description', [], $c),
+        'identifier-text' => $this->t('Identifiers', [], $c),
+        'in-same-series-text' => $this->t('In the same series', [], $c),
+        'number-description-text' => $this->t('Nr.', [], $c),
+        'in-series-text' => $this->t('in the series', [], $c),
+        'login-to-see-review-text' => $this->t('Log in to read the review', [], $c),
+        'cannot-see-review-text' => $this->t('The review is not accessible', [], $c),
+        'rating-text' => $this->t('out of', [], $c),
+        'hearts-icon-text' => $this->t('hearts', [], $c),
+        'details-of-the-material-text' => $this->t('Details of the material', [], $c),
+        'editions-text' => $this->t('Editions', [], $c),
+        'fiction-nonfiction-text' => $this->t('Fiction/nonfiction', [], $c),
+        'details-text' => $this->t('Details', [], $c),
+        'type-text' => $this->t('Type', [], $c),
+        'language-text' => $this->t('Language', [], $c),
+        'contributors-text' => $this->t('Contributors', [], $c),
+        'original-title-text' => $this->t('Original title', [], $c),
+        'isbn-text' => $this->t('ISBN', [], $c),
+        'edition-text' => $this->t('Edition', [], $c),
+        'scope-text' => $this->t('Scope', [], $c),
+        'publisher-text' => $this->t('Publisher', [], $c),
+        'audience-text' => $this->t('Audience', [], $c),
+        'reserve-text' => $this->t('Reserve', [], $c),
+        'available-text' => $this->t('Available', [], $c),
+        'unavailable-text' => $this->t('Unavailable', [], $c),
       ]),
     ];
   }
