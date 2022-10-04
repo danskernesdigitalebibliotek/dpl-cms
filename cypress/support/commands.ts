4e28de4b14782f998b8fdfc93caaa29dd8e34a98
Kasper Garnæs <kasperg@users.noreply.github.com>
2022-10-04 08:42:34 +0200

    Initial integration between Cypress and Wiremock
    Add commands to make integration easier, environment variables which
    work both locally and in Docker and a test to show that it works both
    places.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,55 @@
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { WireMockRestClient } from "wiremock-rest-client";
+import { Options } from "wiremock-rest-client/dist/model/options.model";
+import { StubMapping } from "wiremock-rest-client/dist/model/stub-mapping.model";
+
+const wiremock = (baseUri?: string, options?: Options) => {
+  return new WireMockRestClient(
+    baseUri || Cypress.env("WIREMOCK_URL"),
+    options
+  );
+};
+
+Cypress.Commands.add("createMapping", (stub: StubMapping) => {
+  cy.wrap(wiremock().mappings.createMapping(stub));
+});
+
+Cypress.Commands.add("resetMappings", () => {
+  cy.wrap(wiremock().mappings.resetAllMappings());
+});
+
+Cypress.Commands.add("logMappingRequests", () => {
+  cy.wrap(
+    wiremock()
+      .mappings.getAllMappings()
+      .then((mappings) => {
+        mappings.mappings.forEach((stub) => {
+          wiremock()
+            .requests.getCount(stub.request)
+            .then((request: { count: number }) => {
+              const requestUrlPath =
+                stub.request.url ||
+                stub.request.urlPattern ||
+                stub.request.urlPath ||
+                stub.request.urlPathPattern;
+              Cypress.log({
+                name: "Wiremock",
+                message: `${stub.request.method}: ${requestUrlPath}: ${request.count} hit`,
+              });
+            });
+        });
+      })
+  );
+});
+
+// According to the documentation of types and Cypress commands
+// the namespace is declared like it is done here. Therefore we'll bypass errors about it.
+/* eslint-disable @typescript-eslint/no-namespace */
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      createMapping(stub: StubMapping): Chainable<null>;
+      resetMappings(): Chainable<null>;
+      logMappingRequests(): Chainable<null>;
+    }
+  }
+}
