baee32e3b89ac85143eafcbba54f5b852a6a270c
Kasper Garnæs <kasperg@users.noreply.github.com>
2022-10-05 13:58:34 +0200

    Do not quote keys in Lighthouse configuration
    This is not needed for JavaScript code - only JSON.

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,33 +1,31 @@
 module.exports = {
-  "ci": {
-    "collect": {
-      "url": [
-        'http://varnish:8080/',
-      ],
+  ci: {
+    collect: {
+      url: ["http://varnish:8080/"],
       // Use 3 runs to test both cold and warm caches.
-      "numberOfRuns": 3,
-      "settings": {
-        "chromeFlags": "--no-sandbox",
+      numberOfRuns: 3,
+      settings: {
+        chromeFlags: "--no-sandbox",
         // Lighthouse best practices require HTTPS but we do not this available
         // on our CI environments so disable that check. It should not keep our
         // score down.
-        "skipAudits": ["is-on-https"]
-      }
+        skipAudits: ["is-on-https"],
+      },
     },
-    "assert": {
-      "assertions": {
+    assert: {
+      assertions: {
         // Our quality standard requires all categories to be green. Green
         // translates to a score between 90 and 100 - or 0.9-1.
-        "categories:performance": ["error", {"minScore": 0.9}],
-        "categories:accessibility": ["error", {"minScore": 0.9}],
-        "categories:best-practices": ["error", {"minScore": 0.9}],
-        "categories:seo": ["error", {"minScore": 0.9}]
-      }
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+      },
     },
-    "upload": {
+    upload: {
       // Update to Googles public storage to make reports easily accessible.
       // The fact that the storage is public is fine. The project is open.
-      "target": "temporary-public-storage"
-    }
-  }
+      target: "temporary-public-storage",
+    },
+  },
 };
