e08fcdefc07fa9efbb2e393d0fd195954b471c5b
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-10-13 12:13:14 +0200

    First version of dpl_campaign

---
+++ web/modules/custom/dpl_campaign/tests/src/Unit/CampaignResourceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\Tests\dpl_library_token\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Unit tests for the Library Token Handler.
+ */
+class CampaignResourceTest extends UnitTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {}
+
+  /**
+   * TestThatItValidatesRequiredFacetTermPairsParameter.
+   */
+  public function testThatItValidatesRequiredFacetTermPairsParameter() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * TestThatItValidatesFacetTermPairs.
+   */
+  public function testThatItValidatesFacetTermPairs() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * TestThatItReturnsNotFoundIfThereIsNoCampaignMatch.
+   */
+  public function testThatItReturnsNotFoundIfThereIsNoCampaignMatch() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * TestThatItReturns404IfNoCampaignWasFound.
+   */
+  public function testThatItReturns404IfNoCampaignWasFound() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * TestThatItReturnsAConsumableCampaignObjectIfFound.
+   */
+  public function testThatItReturnsAConsumableCampaignObjectIfFound() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+}
