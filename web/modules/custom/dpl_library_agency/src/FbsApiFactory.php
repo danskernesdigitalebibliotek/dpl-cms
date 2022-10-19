3200e1161a9ff79fd8eea8f28853cf7f914334dd
Kasper Garnæs <kasperg@users.noreply.github.com>
2022-10-19 16:07:23 +0200

    Initial fetching of branches for agency config form

---
+++ web/modules/custom/dpl_library_agency/src/FbsApiFactory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\dpl_library_agency;
+
+use DanskernesDigitaleBibliotek\FBS\Api\ExternalV1AgencyidApi;
+use DanskernesDigitaleBibliotek\FBS\Configuration;
+use Drupal\dpl_library_token\LibraryTokenHandler;
+use GuzzleHttp\ClientInterface;
+
+/**
+ * Factory to generate FBS API instances.
+ */
+class FbsApiFactory {
+
+  /**
+   * The Guzzle client to use to connect to FBS.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $client;
+
+  /**
+   * The token handler to retrieve the library token used for authentication.
+   *
+   * @var \Drupal\dpl_library_token\LibraryTokenHandler
+   */
+  protected $tokenHandler;
+
+  /**
+   * FBS API factory constructor.
+   */
+  public function __construct(ClientInterface $client, LibraryTokenHandler $tokenHandler) {
+    $this->client = $client;
+    $this->tokenHandler = $tokenHandler;
+  }
+
+  /**
+   * Assemble the API configuration.
+   */
+  protected function getConfiguration(): Configuration {
+    $configuration = (new Configuration())
+      ->setHost('https://fbs-openplatform.dbc.dk');
+
+    $token = $this->tokenHandler->getToken();
+    if ($token) {
+      $configuration->setAccessToken($token);
+    }
+
+    return $configuration;
+  }
+
+  /**
+   * Generate an agency API instance.
+   */
+  public function getAgencyApi(): ExternalV1AgencyidApi {
+    return new ExternalV1AgencyidApi(
+      $this->client,
+      $this->getConfiguration()
+    );
+  }
+
+}
--- web/modules/custom/dpl_library_agency/src/Form/GeneralSettingsForm.php
+++ web/modules/custom/dpl_library_agency/src/Form/GeneralSettingsForm.php
@@ -2,17 +2,21 @@
 
 namespace Drupal\dpl_library_agency\Form;
 
+use DanskernesDigitaleBibliotek\FBS\Api\ExternalV1AgencyidApi;
+use DanskernesDigitaleBibliotek\FBS\Model\AgencyBranch;
 use Drupal\Core\Cache\CacheTagsInvalidator;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\dpl_library_agency\ReservationSettings;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use function Safe\array_combine as array_combine;
+use function Safe\sort as sort;
 
 /**
  * General Settings form for a library agency.
  */
 class GeneralSettingsForm extends ConfigFormBase {
-
 
   /**
    * The cache tags invalidator.
@@ -22,12 +26,21 @@ class GeneralSettingsForm extends ConfigFormBase {
   protected $cacheTagsInvalidator;
 
   /**
+   * FBS API instance for fetching branch information.
+   *
+   * @var \DanskernesDigitaleBibliotek\FBS\Api\ExternalV1AgencyidApi
+   */
+  protected $agencyApi;
+
+  /**
    * GeneralSettingsForm constructor.
    */
   public function __construct(
     CacheTagsInvalidator $cacheTagsInvalidator,
+    ExternalV1AgencyidApi $agencyApi,
   ) {
     $this->cacheTagsInvalidator = $cacheTagsInvalidator;
+    $this->agencyApi = $agencyApi;
   }
 
   /**
@@ -41,6 +54,7 @@ class GeneralSettingsForm extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('cache_tags.invalidator'),
+      $container->get('dpl_library_agency.fbs_agency_api')
     );
   }
 
@@ -61,10 +75,38 @@ class GeneralSettingsForm extends ConfigFormBase {
   }
 
   /**
+   * Translates a string to the current language or to a given language.
+   *
+   * @param string $string
+   *   A string containing the English text to translate.
+   * @param mixed[] $args
+   *   Replacements to make after translation. Based on the first character of
+   *   the key, the value is escaped and/or themed.
+   * @param mixed[] $options
+   *   An associative array of additional options.
+   */
+  protected function t($string, array $args = [], array $options = []): TranslatableMarkup {
+    // Intentionally transfer the string originally passed to t().
+    // phpcs:ignore Drupal.Semantics.FunctionT.NotLiteralString
+    return parent::t($string, $args, array_merge($options, ['context' => 'Library Agency Configuration']));
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $config = $this->config('dpl_library_agency.general_settings');
+
+    $branches = $this->agencyApi->getBranches();
+    $branch_options = array_combine(
+      array_map(function (AgencyBranch $branch) {
+        return $branch->getBranchId();
+      }, $branches),
+      array_map(function (AgencyBranch $branch) {
+        return $branch->getTitle();
+      }, $branches)
+    );
+    sort($branch_options);
 
     $form['reservations'] = [
       '#type' => 'fieldset',
@@ -78,6 +120,35 @@ class GeneralSettingsForm extends ConfigFormBase {
       '#title' => $this->t('Disable SMS notifications for reservations'),
       '#default_value' => $config->get('reservation_sms_notifications_disabled'),
       '#description' => $this->t('If checked, SMS notifications for patrons will be disabled.'),
+    ];
+
+    $form['branches'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Branches'),
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+      '#description' => $this->t('Select which branches should be available in different parts of the system.'),
+    ];
+    $form['branches']['search'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Search results'),
+      '#options' => $branch_options,
+      '#default_value' => [],
+      '#description' => $this->t('Only works with holdings belonging to the selected branches will be shown in search results.'),
+    ];
+    $form['branches']['availability'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Availability'),
+      '#options' => $branch_options,
+      '#default_value' => [],
+      '#description' => $this->t('Only holdings belonging to the selected branches will considered when showing work availability.'),
+    ];
+    $form['branches']['reservations'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Reservations'),
+      '#options' => $branch_options,
+      '#default_value' => [],
+      '#description' => $this->t('Only selected branches will be available as pickup locations for reservations.'),
     ];
 
     return parent::buildForm($form, $form_state);
