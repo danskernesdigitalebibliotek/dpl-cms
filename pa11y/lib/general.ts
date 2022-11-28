57d0630779a7412b18fe3e1e530997689b324c20
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-11-28 16:45:42 +0100

    Make sure that query regex is wiremock compatible
    The test tool in the GUI is lying.
    But the wiremock log never lies

--- pa11y/lib/general.ts
+++ pa11y/lib/general.ts
@@ -2,7 +2,7 @@ import { WireMockRestClient } from "wiremock-rest-client";
 import { Options } from "wiremock-rest-client/dist/model/options.model";
 
 export const matchGraphqlQuery = (id: string) =>
-  `$.[?(@.query =~ /.*query ${id}\\(.*/s)]`;
+  `$.[?(@.query=~/.*query ${id}\\(.*/s)]`;
 
 export const wiremock = (baseUri?: string, options?: Options) => {
   return new WireMockRestClient("http://wiremock", options);
