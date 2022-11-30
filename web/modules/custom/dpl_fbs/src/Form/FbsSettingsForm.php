8e6ac453516ed65516b7e0c5f86403b0551c003a
Ture Gjørup <ture@ignatz.dk>
2022-11-30 11:16:58 +0100

    Added basic FSB & Publizon config modules
    Co-Authored-By: Jesper Kristensen <cableman@linuxdev.dk>

---
+++ web/modules/custom/dpl_fbs/src/Form/FbsSettingsForm.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\dpl_fbs\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * FBS setting form.
+ */
+class FbsSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return [
+      'dpl_fbs.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'dpl_fbs_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $config = $this->config('dpl_fbs.settings');
+
+    $form['settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Basic settings'),
+      '#tree' => FALSE,
+    ];
+
+    $form['settings']['base_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('FBS service url'),
+      '#description' => $this->t('Which FBS service should be used (default: https://fbs-openplatform.dbc.dk).'),
+      '#default_value' => $config->get('base_url') ?? 'https://fbs-openplatform.dbc.dk',
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    $url = $form_state->getValue('base_url');
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+      $form_state->setErrorByName('base_url', $this->t('The url "%url" is not a valid URL.', ['%url' => $url]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    parent::submitForm($form, $form_state);
+
+    $this->config('dpl_fbs.settings')
+      ->set('base_url', $form_state->getValue('base_url'))
+      ->save();
+  }
+
+}
