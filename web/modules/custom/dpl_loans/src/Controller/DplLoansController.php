959e3c3e895146afd37dc956e901a1494cb6838a
Ture Gjørup <ture@ignatz.dk>
2022-11-30 13:36:04 +0100

    Code style fixes

--- web/modules/custom/dpl_loans/src/Controller/DplLoansController.php
+++ web/modules/custom/dpl_loans/src/Controller/DplLoansController.php
@@ -12,21 +12,21 @@ class DplLoansController extends ControllerBase {
   /**
    * Demo react rendering.
    *
-   * @return mixed[]
+   * @return array
    *   Render array.
    */
   public function list(): array {
-    $block_manager = \Drupal::service('plugin.manager.block');
+    $block_manager = $this->configFactory->get('plugin.manager.block');
 
     // You can hard code configuration, or you load from settings.
     $config = [];
     $plugin_block = $block_manager->createInstance('dpl_loans_list_block', $config);
 
     // Some blocks might implement access check.
-    $access_result = $plugin_block->access(\Drupal::currentUser());
+    $access_result = $plugin_block->access($this->currentUser());
 
     // Return empty render array if user doesn't have access.
-    // $access_result can be boolean or an AccessResult class
+    // $access_result can be boolean or an AccessResult class.
     if (is_object($access_result) && $access_result->isForbidden() || is_bool($access_result) && !$access_result) {
       // You might need to add some cache tags/contexts.
       return [];
@@ -34,7 +34,7 @@ class DplLoansController extends ControllerBase {
 
     // Add the cache tags/contexts.
     $render = $plugin_block->build();
-    \Drupal::service('renderer')->addCacheableDependency($render, $plugin_block);
+    $this->configFactory->get('renderer')->addCacheableDependency($render, $plugin_block);
 
     return $render;
   }
