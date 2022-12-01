df75232e907e2ccc7634245a9e5b8ccda29581ac
Ture Gjørup <ture@ignatz.dk>
2022-12-01 13:56:02 +0100

    Added update hook and conf

--- web/profiles/dpl_cms/src/Updater/Module.php
+++ web/profiles/dpl_cms/src/Updater/Module.php
@@ -23,4 +23,11 @@ class Module extends UpdaterModule implements UpdaterInterface {
     return 'modules/local';
   }
 
+  function dpl_cms_update_9001() {
+    \Drupal::service('module_installer')->install(['dpl_fbs']);
+    \Drupal::service('module_installer')->install(['dpl_loans']);
+    \Drupal::service('module_installer')->install(['dpl_publizon']);
+    \Drupal::service('module_installer')->install(['dpl_user_profile']);
+  }
+
 }
