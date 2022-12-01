4a65e32c45643087bb2824c1a01e0fb2e79eca27
Ture Gjørup <ture@ignatz.dk>
2022-12-01 13:07:46 +0100

    Fixed copy paste error

--- web/modules/custom/dpl_login/src/Controller/DplLoginController.php
+++ web/modules/custom/dpl_login/src/Controller/DplLoginController.php
@@ -55,7 +55,7 @@ class DplLoginController extends ControllerBase {
   protected $claims;
 
   /**
-   * DdplReactController constructor.
+   * DplLoginController constructor.
    *
    * @param \Drupal\dpl_login\UserTokensProvider $userTokensProvider
    *   The User token provider.
