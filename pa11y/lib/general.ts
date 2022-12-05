43a26df7e2675480e0d7efd5105349997516a01d
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-12-05 10:35:38 +0100

    Use HTTP_PROXY env in in wiremock mapping
    Why not. It is already there...

--- pa11y/lib/general.ts
+++ pa11y/lib/general.ts
@@ -5,7 +5,11 @@ export const matchGraphqlQuery = (id: string) =>
   `$.[?(@.query=~/.*query ${id}\\(.*/s)]`;
 
 export const wiremock = (baseUri?: string, options?: Options) => {
-  return new WireMockRestClient("http://wiremock", options);
+  const wiremockEndpoint = process.env.HTTP_PROXY;
+  if (!wiremockEndpoint) {
+    throw new Error("HTTP_PROXY environment variable is not set");
+  }
+  return new WireMockRestClient(wiremockEndpoint, options);
 };
 
 export default wiremock;
