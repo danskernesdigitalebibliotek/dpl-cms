70b18efec22ff8b9312ac521f3f833b034572f66
Adam Antal <adoantal@gmail.com>
2023-01-16 15:50:17 +0100

    Further develop the user journey cypress test - search result page

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const autosuggestData = require("../../wiremock/src/mappings/search/data/fbi/autosugggest.json");
+const searchResultData = require("../../wiremock/src/mappings/search/data/fbi/searchWithPagination.json");
 
 describe("User journey", () => {
   it("Shows search suggestions & redirects to search result page", () => {
@@ -28,6 +29,32 @@ describe("User journey", () => {
       .click()
       .url()
       .should("include", "search?q=Harry%2520Potter");
+  });
+
+  it("Shows search results & redirects to material page", () => {
+    cy.createMapping({
+      request: {
+        method: "POST",
+        url: "/opac/graphql",
+      },
+      response: {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        status: 200,
+        jsonBody: searchResultData,
+      },
+    });
+
+    cy.visit("/search?q=Harry%2520Potter")
+      .get('[data-cy="search-result-title"]')
+      .should("contain", "Showing results for “Harry Potter” (109)")
+      .get('[data-cy="search-result-list"]')
+      .children()
+      .eq(0)
+      .click()
+      .url()
+      .should("include", "work/work-of:870970-basis:25245784");
   });
 
   beforeEach(() => {
