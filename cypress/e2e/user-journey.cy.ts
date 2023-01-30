9d4e1b96f0a047f940c08246aac6b0ebb6d674a2
Adam Antal <adoantal@gmail.com>
2023-01-30 14:41:48 +0100

    Don't mock calls inside the user joyrney cypress test
    We have a command that does it automatically for us - ci:cypress
    and ci:cypress:local both utilize ci:wiremock:create-mappings

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -1,22 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const autosuggestData = require("../../wiremock/src/mappings/search/data/fbi/autosugggest.json");
-
 describe("User journey", () => {
   it("Shows search suggestions & redirects to search result page", () => {
-    cy.createMapping({
-      request: {
-        method: "POST",
-        url: "/opac/graphql",
-      },
-      response: {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        status: 200,
-        jsonBody: autosuggestData,
-      },
-    });
-
     cy.visit("/")
       .get(".header__menu-search-input")
       .focus()
