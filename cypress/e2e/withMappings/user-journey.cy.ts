934a2345c803234f51b083e68736ce1b227ec6c1
Adam Antal <adoantal@gmail.com>
2023-01-31 13:55:24 +0100

    Add search page e2e test into the user journey test

--- cypress/e2e/withMappings/user-journey.cy.ts
+++ cypress/e2e/withMappings/user-journey.cy.ts
@@ -13,6 +13,18 @@ describe("User journey", () => {
       .should("include", "search?q=Harry%2520Potter");
   });
 
+  it("Shows search results & redirects to material page", () => {
+    cy.visit("/search?q=Harry%2520Potter")
+      .getBySel("search-result-title")
+      .should("contain", "Showing results for “Harry Potter” (109)")
+      .getBySel("search-result-list")
+      .children()
+      .first()
+      .click()
+      .url()
+      .should("include", "work/work-of:870970-basis:25245784");
+  });
+
   afterEach(() => {
     cy.logMappingRequests();
   });
