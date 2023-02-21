afc66d8f147392b166cd9aeff87bc1613b8c01e1
Jeppe Krogh <kjej@aarhus.dk>
2023-02-21 11:42:24 +0100

    added translation

--- web/modules/custom/dpl_menu/src/Plugin/Block/MenuBlock.php
+++ web/modules/custom/dpl_menu/src/Plugin/Block/MenuBlock.php
@@ -65,8 +65,8 @@ class MenuBlock extends BlockBase implements ContainerFactoryPluginInterface
    */
   public function build()
   {
-    $context = ['context' => 'menu list'];
-    $contextAria = ['context' => 'menu list (Aria)'];
+    $context = ["context" => 'menu list'];
+    $contextAria = ["context" => 'menu list (Aria)'];
 
     $fbsConfig = $this->configFactory->get('dpl_fbs.settings');
     $publizonConfig = $this->configFactory->get('dpl_publizon.settings');
@@ -82,51 +82,31 @@ class MenuBlock extends BlockBase implements ContainerFactoryPluginInterface
         "page-size-mobile" => "25",
         // Urls.
         // @todo update placeholder URL's
-        // 'menu-page-url' => "https://unsplash.com/photos/wd6YQy0PJt8",
-        // 'material-overdue-url' => "https://unsplash.com/photos/wd6YQy0PJt8",
-        'search-url' => DplReactAppsController::searchResultUrl(),
-        'dpl-cms-base-url' => DplReactAppsController::dplCmsBaseUrl(),
+        // 'menu-page-url" => "https://unsplash.com/photos/wd6YQy0PJt8",
+        // 'material-overdue-url" => "https://unsplash.com/photos/wd6YQy0PJt8",
+        "search-url" => DplReactAppsController::searchResultUrl(),
+        "dpl-cms-base-url" => DplReactAppsController::dplCmsBaseUrl(),
         // Texts.
-        'intermediate-list-headline-text' => $this->t("menu & replacement costs", [], $context),
-        'intermediate-list-body-text' => $this->t("overdue menu and replacement costs that were created before 27/10/2020 can still be paid on this page.", [], $context),
-        'view-menu-and-compensation-rates-text' => $this->t("see our menu and replacement costs", [], $context),
-        'material-and-author-text' => $this->t("and", [], $context),
-        'total-fee-amount-text' => $this->t("Fee", [], $context),
-        'other-materials-text' => $this->t("Other materials", [], $context),
-        'material-by-author-text' => $this->t("By", [], $context),
-        'intermediate-list-days-text' => $this->t("Days", [], $context),
-        'pay-text' => $this->t("Pay", [], $context),
-        'total-text' => $this->t("Total", [], $context),
-        'i-accept-text' => $this->t("I accept the", [], $context),
-        'terms-of-trade-text' => $this->t("Terms of trade", [], $context),
-        'unpaid-menu-text' => $this->t("Unsettled debt", [], $context),
-        'pre-payment-type-change-date-text' => $this->t("BEFORE 27/10 2020", [], $context),
-        'post-payment-type-change-date-text' => $this->t("AFTER 27/10 2020", [], $context),
-        'already-paid-text' => $this->t("Please note that paid menu are not registered up until 72 hours after your payment after which your debt is updated and your user unblocked if it has been blocked.", [], $context),
-        'intermediate-payment-modal-header-text' => $this->t("Unpaid menu post 27/10 2020", [], $context),
-        'intermediate-payment-modal-body-text' => $this->t("You will be redirected to Mit Betalingsoverblik.", [], $context),
-        'intermediate-payment-modal-notice-text' => $this->t("Paid menu can take up to 24 hours to registrer.", [], $context),
-        'intermediate-payment-modal-goto-text' => $this->t("Go to Mit Betalingsoverblik", [], $context),
-        'intermediate-payment-modal-cancel-text' => $this->t("Cancel", [], $context),
-        'fee-details-modal-screen-reader-text' => $this->t("A modal containing details about a fee", [], $context),
-        'empty-intermediate-list-text' => $this->t("You have 0 unpaid menu or replacement costs", [], $context),
-        'fee-details-modal-close-modal-aria-label-text' => $this->t("Close fee details modal", [], $contextAria),
-        'fee-details-modal-description-text' => $this->t("Modal containing information about this element or group of elements menu", [], $context),
-        'turned-in-text' => $this->t("Turned in @date", [], $context),
-        'plus-x-other-materials-text' => $this->t("+ @amount other materials", [], $context),
-        'item-fee-amount-text' => $this->t("Fee @fee,-", [], $context),
-        'fee-created-text' => $this->t("menu charged @date", [], $context),
-
-        'available-payment-types-url' => $this->t("https://unsplash.com/photos/JDzoTGfoogA", [], $context),
-        'payment-overview-url' => $this->t("https://unsplash.com/photos/yjI3ozta2Zk", [], $context),
-        'view-menu-and-compensation-rates-url' => $this->t("https://unsplash.com/photos/NEJcmvLFcws", [], $context),
-        'terms-of-trade-url' => $this->t("https://unsplash.com/photos/JDzoTGfoogA", [], $context),
+        "menu-view-your-profile-text" => $this->t("My Account", [], $context),
+        "menu-view-your-profile-text-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+        "menu-notification-loans-expired-text" => $this->t("loans expired", [], $context),
+        "menu-notification-loans-expired-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+        "menu-notification-loans-expiring-soon-text" => $this->t("loans expiring soon", [], $context),
+        "menu-notification-loans-expiring-soon-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+        "menu-notification-ready-for-pickup-text" => $this->t("reservations ready for pickup", [], $context),
+        "menu-notification-ready-for-pickup-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+        "menu-log-out-text" => $this->t("Log Out", [], $context),
+        "menu-log-out-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+        "menu-login-text" => $this->t("Log in", [], $context),
+        "menu-login-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+        "menu-sign-up-text" => $this->t("Sign up", [], $context),
+        "menu-sign-up-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
         ] + DplReactAppsController::externalApiBaseUrls();
 
     $app = [
-      '#theme' => 'dpl_react_app',
-      "#name" => 'menu',
-      '#data' => $data,
+      "#theme" => "dpl_react_app",
+      "#name" => "menu",
+      "#data" => $data,
     ];
     return $app;
   }
