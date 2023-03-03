ad8cbc5aac741b927b0df36a2c90a9fa15ca6de3
Jeppe Krogh <kjej@aarhus.dk>
2023-03-03 14:15:37 +0100

    adding menu module

---
+++ web/modules/custom/dpl_menu/src/Form/MenuSettingsForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\dpl_menu\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Menu setting form.
+ */
+class MenuSettingsForm extends ConfigFormBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array
+  {
+    return [
+      'dpl_menu.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string
+  {
+    return 'menu_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array
+  {
+    $config = $this->config('dpl_menu.settings');
+
+    $form['settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Basic settings'),
+      '#tree' => FALSE,
+    ];
+
+
+    $form['settings']['menu_navigation_data_config'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Menu Navigation Data Config'),
+      '#description' => $this->t('JSON definition of menu data'),
+      '#default_value' => $config->get('menu_navigation_data_config') ?? '',
+    ];
+
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void
+  {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void
+  {
+    parent::submitForm($form, $form_state);
+
+    $this->config('dpl_menu.settings')
+      ->set('menu_navigation_data_config', $form_state->getValue('menu_navigation_data_config'))
+      ->save();
+  }
+}
---
+++ web/modules/custom/dpl_menu/src/Plugin/Block/MenuBlock.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Drupal\dpl_menu\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\dpl_react_apps\Controller\DplReactAppsController;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides user intermediate list.
+ *
+ * @Block(
+ *   id = "dpl_menu_block",
+ *   admin_label = "List user menu"
+ * )
+ */
+class MenuBlock extends BlockBase implements ContainerFactoryPluginInterface
+{
+  /**
+   * Drupal config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private ConfigFactoryInterface $configFactory;
+
+  /**
+   * MenuBlock constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Drupal config factory to get FBS and Publizon settings.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $configFactory)
+  {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configuration = $configuration;
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return mixed[]
+   *   The app render array.
+   */
+  public function build()
+  {
+    $context = ["context" => 'menu list'];
+    $contextAria = ["context" => 'menu list (Aria)'];
+
+    $fbsConfig = $this->configFactory->get('dpl_fbs.settings');
+    $publizonConfig = $this->configFactory->get('dpl_publizon.settings');
+
+    $data = [
+      // Config.
+      "threshold-config" => $this->configFactory->get('dpl_library_agency.general_settings')->get('threshold_config'),
+      // "menu-navigation-data-config" => $this->configFactory->get('dpl_menu.settings')->get('menu_navigation_data_config'),
+      "menu-navigation-data-config" => '[{"name": "Loans","link": "","dataId": "1"},{"name": "Reservations","link": "","dataId": "2"},{"name": "My list","link": "","dataId": "3"},{"name": "Fees & Replacement costs","link": "","dataId": "4"},{"name": "My account","link": "","dataId": "5"}]',
+      "fbs-base-url" => $fbsConfig->get('base_url'),
+      "publizon-base-url" => $publizonConfig->get('base_url'),
+      "page-size-desktop" => "25",
+      "page-size-mobile" => "25",
+      // Urls.
+      // @todo update placeholder URL's
+      // 'menu-page-url" => "https://unsplash.com/photos/wd6YQy0PJt8",
+      // 'material-overdue-url" => "https://unsplash.com/photos/wd6YQy0PJt8",
+      "search-url" => DplReactAppsController::searchResultUrl(),
+      "dpl-cms-base-url" => DplReactAppsController::dplCmsBaseUrl(),
+
+      // Texts.
+      "menu-view-your-profile-text" => $this->t("My Account", [], $context),
+      "menu-view-your-profile-text-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+      "menu-notification-loans-expired-text" => $this->t("loans expired", [], $context),
+      "menu-notification-loans-expired-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+      "menu-notification-loans-expiring-soon-text" => $this->t("loans expiring soon", [], $context),
+      "menu-notification-loans-expiring-soon-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+      "menu-notification-ready-for-pickup-text" => $this->t("reservations ready for pickup", [], $context),
+      "menu-notification-ready-for-pickup-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+      "menu-log-out-text" => $this->t("Log Out", [], $context),
+      "menu-log-out-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+      "menu-login-text" => $this->t("Log in", [], $context),
+      "menu-login-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+      "menu-sign-up-text" => $this->t("Sign up", [], $context),
+      "menu-sign-up-url" => $this->t("https://unsplash.com/photos/tNJdaBc-r5c", [], $context),
+    ] + DplReactAppsController::externalApiBaseUrls();
+
+    $app = [
+      "#theme" => "dpl_react_app",
+      "#name" => "menu",
+      "#data" => $data,
+    ];
+    return $app;
+  }
+}
+
