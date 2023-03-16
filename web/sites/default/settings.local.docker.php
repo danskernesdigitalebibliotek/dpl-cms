72705e9ece37fc8f03e78a3ed72d16e43363319c
Jesper Kristensen <cableman@linuxdev.dk>
2023-03-16 19:41:08 +0100

    ITK: Re-added docker local settings file

---
+++ web/sites/default/settings.local.docker.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array(
+ 'database' => 'db',
+ 'username' => 'db',
+ 'password' => 'db',
+ 'host' => 'mariadb',
+ 'port' => '',
+ 'driver' => 'mysql',
+ 'prefix' => '',
+);
+
