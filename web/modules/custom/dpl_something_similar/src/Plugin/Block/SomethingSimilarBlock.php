f4eecbfb80926a03cbd81eff33adc44859e77b07
Sine Jespersen <sinejespersen@gmail.com>
2023-04-14 12:54:17 +0200

    CMN-371: something similar module

---
+++ web/modules/custom/dpl_something_similar/src/Plugin/Block/SomethingSimilarBlock.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Drupal\dpl_something_similar\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\dpl_react_apps\Controller\DplReactAppsController;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a "something similar" component.
+ *
+ * @Block(
+ *   id = "dpl_something_similar_block",
+ *   admin_label = "Something similar"
+ * )
+ */
+class SomethingSimilarBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private ConfigFactoryInterface $configFactory;
+
+  /**
+   * SomethingSimilarBlock constructor.
+   *
+   * @param mixed[] $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Drupal config factory to get FBS and Publizon settings.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $configFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configuration = $configuration;
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The service container.
+   * @param mixed[] $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param int $plugin_definition
+   *   The plugin implementation definition.
+   *
+   * @return \Drupal\dpl_something_similar\Plugin\Block\SomethingSimilarBlock|static
+   *   Something similar. 
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return mixed[]
+   *   The app render array.
+   */
+  public function build() {
+    $fbsConfig = $this->configFactory->get('dpl_fbs.settings');
+    $publizonConfig = $this->configFactory->get('dpl_publizon.settings');
+    $data = [
+      // Urls.
+      "fbs-base-url" => $fbsConfig->get('base_url'),
+      "publizon-base-url" => $publizonConfig->get('base_url'),
+      'dpl-cms-base-url' => DplReactAppsController::dplCmsBaseUrl(),
+      'material-url' => DplReactAppsController::materialUrl(),
+      'something-similar-title-text' => $this->t("Other materials", [], ['context' => 'Something similar']),
+      'material-by-author-text' => $this->t("By", [], ['context' => 'Something similar']),
+      'material-and-author-text' => $this->t("and", [], ['context' => 'Something similar']),
+      'something-similar-by-the-same-author-text' => $this->t("By the same author", [], ['context' => 'Something similar']),
+      'something-similar-something-similar-author-text' => $this->t("Something similar", [], ['context' => 'Something similar']),
+      'add-to-favorites-aria-label-text' => $this->t("Add element to favorites list", [], ['context' => 'Something similar (Aria)']),
+      'remove-from-favorites-aria-label-text' => $this->t("Remove element from favorites list", [], ['context' => 'Something similar (Aria)']),
+      'faust' => self::faustFromUrl(),
+    ] + DplReactAppsController::externalApiBaseUrls();
+
+    return [
+      '#theme' => 'dpl_react_app',
+      "#name" => 'something-similar',
+      '#data' => $data,
+    ];
+  }
+
+
+
+  /**
+   * Get faust from url.
+   */
+  public static function faustFromUrl(): string {
+    return \Drupal::routeMatch()->getParameters()->get('faust');
+  }
+
+
+}
