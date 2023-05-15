6bf109398dac3633d8cc2199cff97e1f9725d9c6
Sine Jespersen <sinejespersen@gmail.com>
2023-05-15 13:51:08 +0200

    CMN-577: remove unecessary validateform

--- web/modules/custom/dpl_favorites_list_material_component/src/Form/FavoritesListMaterialComponentSettingsForm.php
+++ web/modules/custom/dpl_favorites_list_material_component/src/Form/FavoritesListMaterialComponentSettingsForm.php
@@ -51,12 +51,6 @@ class FavoritesListMaterialComponentSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state): void {
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
     parent::submitForm($form, $form_state);
 
