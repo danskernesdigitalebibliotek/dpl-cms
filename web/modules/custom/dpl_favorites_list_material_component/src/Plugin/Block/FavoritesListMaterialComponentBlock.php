5b87a8da7cc672ff17c4a983ecb5d0c5d8ab503c
Jesper Kristensen <cableman@linuxdev.dk>
2023-05-19 13:44:23 +0200

    ITK: Sorted proptypes in fav. list comp.

--- web/modules/custom/dpl_favorites_list_material_component/src/Plugin/Block/FavoritesListMaterialComponentBlock.php
+++ web/modules/custom/dpl_favorites_list_material_component/src/Plugin/Block/FavoritesListMaterialComponentBlock.php
@@ -78,16 +78,15 @@ class FavoritesListMaterialComponentBlock extends BlockBase implements Container
 
     $data = [
       // Urls.
-      'favorites-list-material-component-go-to-list-url' => $favoritesListMaterialComponentSettings->get('favorites_list_url'),
       'dpl-cms-base-url' => DplReactAppsController::dplCmsBaseUrl(),
+      'favorites-list-material-component-go-to-list-url' => $favoritesListMaterialComponentSettings->get('favorites_list_url'),
       // Texts.
-      'favorites-list-material-component-title-text' => $this->t("Your list", [], ['context' => 'Favorites list material component']),
-      'material-by-author-text' => $this->t("By", [], ['context' => 'Favorites list material component']),
-      'material-and-author-text' => $this->t("and", [], ['context' => 'Favorites list material component']),
-      'favorites-list-material-component-go-to-list-text' => $this->t("Go to My list", [], ['context' => 'Favorites list material component']),
       'add-to-favorites-aria-label-text' => $this->t("Add element to favorites list", [], ['context' => 'Favorites list material component (aria)']),
+      'favorites-list-material-component-go-to-list-text' => $this->t("Go to My list", [], ['context' => 'Favorites list material component']),
+      'favorites-list-material-component-title-text' => $this->t("Your list", [], ['context' => 'Favorites list material component']),
+      'material-and-author-text' => $this->t("and", [], ['context' => 'Favorites list material component']),
+      'material-by-author-text' => $this->t("By", [], ['context' => 'Favorites list material component']),
       'remove-from-favorites-aria-label-text' => $this->t("Remove element from favorites list", [], ['context' => 'Favorites list material component (aria)']),
-
     ] + DplReactAppsController::externalApiBaseUrls();
 
     return [
