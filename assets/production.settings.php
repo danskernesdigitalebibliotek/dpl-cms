43bf34b8ea1f08dc12aab1ad00131571d98e648c
Mikkel Jakobsen <mikkel@reload.dk>
2023-06-06 11:57:24 +0200

    Revert "Enable cron in non-production environments through automated cron"
    This reverts commit d1f5c7dcac4adfec4e998158cc4a16db0201086c.

--- assets/production.settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Disable automated cron for production environment.
-// In production Lagoon will execute cron in the background as defined in
-// .lagoon.yml. This is currently not supported for pull request environments
-// so here we rely on Drupals Automated Cron module for cron execution.
-$config['automated_cron.settings']['interval'] = 0;
