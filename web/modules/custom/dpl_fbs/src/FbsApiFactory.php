ec1b9d1fe40238db936bdced37c95fa6a9568cf0
Kasper Garnæs <kasperg@users.noreply.github.com>
2023-10-11 21:31:57 +0200

    Refactor token handling for FBI API factory
    We need to support both agency and user tokens for different APIs
    
    By removing the library token handler from the API factory arguments
    and replacing it with a simple string representing the token which will
    be used for individual API instances we get the flexibility we need and
    drop any direct dependencies between the DPL Library Token module.
    
    This means we have to update handling and dependencies in the library 
    agency module accordingly.

--- web/modules/custom/dpl_fbs/src/FbsApiFactory.php
+++ web/modules/custom/dpl_fbs/src/FbsApiFactory.php
@@ -6,7 +6,6 @@ use DanskernesDigitaleBibliotek\FBS\Api\ExternalV1AgencyidApi;
 use DanskernesDigitaleBibliotek\FBS\Configuration;
 use Drupal\Core\Config\ConfigManagerInterface;
 use Drupal\dpl_fbs\Form\FbsSettingsForm;
-use Drupal\dpl_library_token\LibraryTokenHandler;
 use GuzzleHttp\ClientInterface;
 
 /**
@@ -21,28 +20,22 @@ class FbsApiFactory {
    *   The config manager.
    * @param \GuzzleHttp\ClientInterface $client
    *   The Guzzle client to use to connect to FBS.
-   * @param \Drupal\dpl_library_token\LibraryTokenHandler $tokenHandler
-   *   The token handler to retrieve the library token used for authentication.
    */
   public function __construct(
     protected ConfigManagerInterface $configManager,
     protected ClientInterface $client,
-    protected LibraryTokenHandler $tokenHandler
   ) {
   }
 
   /**
    * Assemble the API configuration.
    */
-  protected function getConfiguration(): Configuration {
+  protected function getConfiguration(string $token): Configuration {
     $config = $this->configManager->getConfigFactory()->get(FbsSettingsForm::CONFIG_KEY);
-    $configuration = (new Configuration())
-      ->setHost($config->get('base_url'));
 
-    $token = $this->tokenHandler->getToken();
-    if ($token) {
-      $configuration->setAccessToken($token);
-    }
+    $configuration = (new Configuration())
+      ->setHost($config->get('base_url'))
+      ->setAccessToken($token);
 
     return $configuration;
   }
@@ -50,10 +43,10 @@ class FbsApiFactory {
   /**
    * Generate an agency API instance.
    */
-  public function getAgencyApi(): ExternalV1AgencyidApi {
+  public function getAgencyApi(string $token): ExternalV1AgencyidApi {
     return new ExternalV1AgencyidApi(
       $this->client,
-      $this->getConfiguration()
+      $this->getConfiguration($token)
     );
   }
 
--- web/modules/custom/dpl_library_agency/src/Branch/FbsBranchRepository.php
+++ web/modules/custom/dpl_library_agency/src/Branch/FbsBranchRepository.php
@@ -2,8 +2,9 @@
 
 namespace Drupal\dpl_library_agency\Branch;
 
-use DanskernesDigitaleBibliotek\FBS\Api\ExternalV1AgencyidApi;
 use DanskernesDigitaleBibliotek\FBS\Model\AgencyBranch;
+use Drupal\dpl_fbs\FbsApiFactory;
+use Drupal\dpl_library_token\LibraryTokenHandler;
 
 /**
  * Retrieves agency branches from the FBS external service.
@@ -13,12 +14,16 @@ class FbsBranchRepository implements BranchRepositoryInterface {
   /**
    * External branch API constructor.
    *
-   * @param \DanskernesDigitaleBibliotek\FBS\Api\ExternalV1AgencyidApi $api
-   *   The API instance to use to integrate with FBS.
+   * @param \Drupal\dpl_fbs\FbsApiFactory $apiFactory
+   *   API factory to setup the agency API.
+   * @param \Drupal\dpl_library_token\LibraryTokenHandler $tokenHandler
+   *   The token handler to use to retrieve the library token.
    */
   public function __construct(
-    private ExternalV1AgencyidApi $api,
-  ) {}
+    private FbsApiFactory $apiFactory,
+    private LibraryTokenHandler $tokenHandler,
+  ) {
+  }
 
   /**
    * {@inheritdoc}
@@ -26,9 +31,12 @@ class FbsBranchRepository implements BranchRepositoryInterface {
    * @throws \DanskernesDigitaleBibliotek\FBS\ApiException
    */
   public function getBranches(): array {
+    // Use an empty token if we don't have one. This may cause the API to throw
+    // an exception, but that is acceptable.
+    $api = $this->apiFactory->getAgencyApi($this->tokenHandler->getToken() ?? '');
     return array_map(function (AgencyBranch $branch) {
       return new Branch($branch->getBranchId(), $branch->getTitle());
-    }, $this->api->getBranches());
+    }, $api->getBranches());
   }
 
 }
