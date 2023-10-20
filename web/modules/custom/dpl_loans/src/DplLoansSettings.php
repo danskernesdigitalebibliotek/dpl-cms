31bb0c4a3545875b9eca3e0f2fd530a20af88363
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-10-20 20:47:28 +0200

    Adding defaults for loan settings

--- web/modules/custom/dpl_loans/src/DplLoansSettings.php
+++ web/modules/custom/dpl_loans/src/DplLoansSettings.php
@@ -9,6 +9,11 @@ use Drupal\dpl_react\DplReactConfigBase;
  */
 class DplLoansSettings extends DplReactConfigBase {
 
+  const MATERIAL_OVERDUE_URL = '<front>';
+  const PAGE_SIZE_DESKTOP = 25;
+  const PAGE_SIZE_MOBILE = 25;
+
+
   /**
    * Gets the configuration key for loans settings.
    */
