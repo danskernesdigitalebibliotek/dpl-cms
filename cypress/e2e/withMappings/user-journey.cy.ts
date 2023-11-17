23658b014b2534634cb71d4d3d8cd24c3b826505
Kasper Garnæs <kasperg@users.noreply.github.com>
2023-11-17 12:06:16 +0100

    Reeable tests which require login with Adgangsplatformen
    Mocked login works again so we can reenable these tests.

--- cypress/e2e/withMappings/user-journey.cy.ts
+++ cypress/e2e/withMappings/user-journey.cy.ts
@@ -41,9 +41,7 @@ describe("User journey", () => {
     );
   });
 
-  // Test is failing because of an "access denied" error after end authentication.
-  // TODO: Fix this test.
-  it.skip("Can open reservation modal & reserve a material", () => {
+  it("Can open reservation modal & reserve a material", () => {
     const authorizationCode = "7c5e3213aea6ef42ec97dfeaa6f5b1d454d856dc";
     const accessToken = "447131b0a03fe0421204c54e5c21a60d70030fd1";
     const userGuid = "19a4ae39-be07-4db9-a8b7-8bbb29f03da6";
