445d0c094034ece5d555c06704b73c61fc13ee58
LasseStaus <l.stausgaard@gmail.com>
2024-01-05 14:40:25 +0100

    Rewrite ticket price ranges for events. Moved logic out of .theme
    The preprocess for handling the field_ticket_categoris for events  was located in theme folder, but is now moved into the dpl_event module.
    
    The logic for calculating the price range was moved aswell, into the priceFormatter.php file and is now a method on that class instead. this makes it more  resuable across the project and is a more intuitive placement.
    
    DDFFORM-59

--- web/modules/custom/dpl_event/src/PriceFormatter.php
+++ web/modules/custom/dpl_event/src/PriceFormatter.php
@@ -4,6 +4,7 @@ namespace Drupal\dpl_event;
 
 use Brick\Math\BigDecimal;
 use Drupal\Core\StringTranslation\TranslationInterface;
+use function Safe\sort;
 
 /**
  * Formats prices according to local rules.
@@ -46,6 +47,67 @@ class PriceFormatter {
       }
     }
     return $price_string;
+  }
+
+  /**
+   * Formats a range of numeric prices into a string.
+   *
+   * Sorts and formats prices, prepending "Free" if applicable.
+   * Outputs formats like "Free - 20 kr.", "20 - 50 kr.", or just "Free".
+   *
+   * @param float[]|int[] $prices
+   *   Array of price values (numbers).
+   *
+   * @return string
+   *   Formatted price range string.
+   */
+  public function formatPriceRange(array $prices): string {
+    sort($prices);
+
+    // Check if all prices are zero (free) or the array is empty.
+    if (empty($prices) || max($prices) == 0) {
+      return $this->translation->translate("Free");
+    }
+
+    $has_free_price = in_array(0, $prices);
+    // Remove free prices (0 values) for further processing.
+    $filtered_prices = array_filter($prices, fn($price) => $price > 0);
+
+    // Format the price range.
+    $price_range = array_map(fn($price) => $this->formatRawPrice((string) $price), $filtered_prices);
+    $formatted_price_range = implode(' - ', $price_range);
+
+    // Prepend "Free" if there are free prices and other prices.
+    if ($has_free_price && !empty($filtered_prices)) {
+      $formatted_price_range = $this->translation->translate("Free") . ' - ' . $formatted_price_range;
+    }
+
+    return $formatted_price_range . ' kr.';
+  }
+
+  /**
+   * Helper function to format a raw price number.
+   *
+   * If the price has a fractional part, it will format to two decimal places,
+   * if not, it is converted to an integer.
+   *
+   * @param string $price_string
+   *   Raw price string.
+   *
+   * @return string
+   *   Formatted price number without currency suffix.
+   */
+  protected function formatRawPrice(string $price_string): string {
+    $price = BigDecimal::of($price_string);
+
+    if ($price->hasNonZeroFractionalPart()) {
+      // Format the price with two decimal places.
+      return number_format($price->toFloat(), 2, ',', '.');
+    }
+    else {
+      // Convert the price to an integer string.
+      return (string) $price->toInt();
+    }
   }
 
 }
