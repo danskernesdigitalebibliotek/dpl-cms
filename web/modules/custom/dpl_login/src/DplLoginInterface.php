4f9b0e07ecb772f2a12b6beaa10f925a9b5d3c86
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-02-20 16:52:13 +0100

    Removing uneccessary params and return documentation
    And add a new logger key instead of string concat

--- web/modules/custom/dpl_login/src/DplLoginInterface.php
+++ web/modules/custom/dpl_login/src/DplLoginInterface.php
@@ -9,5 +9,6 @@ interface DplLoginInterface {
 
   const ROLE_PATRON = 'patron';
   const LOGGER_KEY = 'dpl_login';
+  const LOGGER_KEY_UNREGISTERED_USER = 'dpl_login.unregistered_user';
 
 }
--- web/modules/custom/dpl_login/src/UserTokensProviderInterface.php
+++ web/modules/custom/dpl_login/src/UserTokensProviderInterface.php
@@ -3,31 +3,22 @@
 namespace Drupal\dpl_login;
 
 /**
- * Interface for dpl_login constants.
+ * Interface for user access tokens.
  */
 interface UserTokensProviderInterface {
 
   /**
    * Set access token.
-   *
-   * @param \Drupal\dpl_login\AccessToken $accessToken
-   *   Good old access token.
    */
   public function setAccessToken(AccessToken $accessToken): void;
 
   /**
-   * Get access token.
-   *
-   * @return \Drupal\dpl_login\AccessToken|null
-   *   Accesstoken or NULL if no one has been stored.
+   * Get access token or NULL if no one has been stored.
    */
   public function getAccessToken(): ?AccessToken;
 
   /**
    * Delete access token.
-   *
-   * @return bool
-   *   Was the token successfully deleted?
    */
   public function deleteAccessToken(): bool;
 
