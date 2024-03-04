1f7025aed68f8364b4950dce461564a0637aae64
Benjamin Rasmussen <benjamin@reload.dk>
2024-03-04 10:18:54 +0100

    Move event description from WYSIWYG to plain textarea. DDFFORM-298
    - Add warning on existing WYSIWYG field, and hide it from editors
    - Setup basic field_description text area field
    - Create deploy hook, moving data from WYSIWYG to textarea
    - Delete existing data in WYSIWYG
    - In a future deploy, we'll delete the WYSIWYG field.

---
+++ web/modules/custom/dpl_event/dpl_event.deploy.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @file
+ * Event deploy hooks.
+ *
+ * These get run AFTER config-import.
+ */
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+
+/**
+ * Pre-populate data to new non-WYSIWYG field field_description.
+ *
+ * We also empty out the old field. The field has been set to be hidden
+ * from the editors, and we'll delete it in a future deploy.
+ */
+function dpl_event_deploy_port_description_fields() : string {
+  $message = _dpl_event_port_wysiwyg('eventseries', 'field_event_description', 'field_description');
+  $message .= _dpl_event_port_wysiwyg('eventinstance', 'field_event_description', 'field_description');
+
+  return $message;
+}
+
+/**
+ * A helper function, for porting WYSIWYG fields to plain textareas.
+ */
+function _dpl_event_port_wysiwyg(string $entity_type, string $source_field, string $target_field): string {
+  $ids =
+    \Drupal::entityQuery($entity_type)
+      ->condition($source_field, '', '<>')
+      ->accessCheck(FALSE)
+      ->execute();
+
+  $entities =
+    \Drupal::entityTypeManager()->getStorage($entity_type)->loadMultiple($ids);
+
+  foreach ($entities as $entity) {
+    if (!($entity instanceof FieldableEntityInterface) || !$entity->hasField($target_field)) {
+      continue;
+    }
+
+    $value = $entity->get($source_field)->getValue();
+    $text = $value[0]['value'] ?? '';
+    $text = strip_tags($text);
+
+    $entity->set($target_field, $text);
+    $entity->set($source_field, NULL);
+    $entity->save();
+  }
+
+  return t("Updated @count description fields on @entity_type \r\n", [
+    "@count" => count($entities),
+    "@entity_type" => $entity_type,
+  ])->render();
+}
