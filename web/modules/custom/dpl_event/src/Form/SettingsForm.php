85232170a5f5ced2249cc5610658395fa5398be2
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-03-07 16:37:07 +0100

    Expose event config name
    This reduces duplication and makes it possible to reference the value
    across the codebase.

--- web/modules/custom/dpl_event/src/Form/SettingsForm.php
+++ web/modules/custom/dpl_event/src/Form/SettingsForm.php
@@ -16,6 +16,8 @@ use function Safe\array_combine as array_combine;
  */
 final class SettingsForm extends ConfigFormBase {
 
+  const CONFIG_NAME = 'dpl_event.settings';
+
   /**
    * Constructor for the settings form.
    */
@@ -40,21 +42,21 @@ final class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId(): string {
-    return 'dpl_event_settings';
+    return self::CONFIG_NAME;
   }
 
   /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames(): array {
-    return ['dpl_event.settings'];
+    return [self::CONFIG_NAME];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-    $config = $this->config('dpl_event.settings');
+    $config = $this->config(self::CONFIG_NAME);
 
     $form['unpublish'] = [
       '#type' => 'fieldset',
@@ -99,7 +101,7 @@ final class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
-    $this->config('dpl_event.settings')
+    $this->config(self::CONFIG_NAME)
       ->set('unpublish_schedule', $form_state->getValue('unpublish_schedule'))
       ->save();
     parent::submitForm($form, $form_state);
