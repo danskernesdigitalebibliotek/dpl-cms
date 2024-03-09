003ee60c84e82c0471f66912fd21fe4db9375cfa
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-03-09 11:09:14 +0100

    Add description of the purpose of the custom CtpConfigManager class

--- web/modules/custom/dpl_po/src/Services/CtpConfigManager.php
+++ web/modules/custom/dpl_po/src/Services/CtpConfigManager.php
@@ -9,7 +9,18 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 /**
  * Extended CtpConfigManager in order to handle untranslated strings properly.
  *
- * @package Drupal\config_translation_po\Services
+ * The class has been created to override the behavior of the original class.
+ * The original class sets untranslated strings to the original value,
+ * which is not correct.
+ * Untranslated strings should be set to an empty string to make it possible
+ * for external tools to know that this string is translatable.
+ *
+ * We could also have solved this by creating a patch for the original class.
+ * It is worthwhile to raise this issue with the original module maintainers
+ * and keep an eye of the issue queue.
+ *
+ * Apparently this is where things went wrong:
+ * https://git.drupalcode.org/project/config_translation_po/-/commit/f9f952730e548418586a6e543da82acaf5042a08
  */
 class CtpConfigManager extends OrgCtpConfigManager {
 
