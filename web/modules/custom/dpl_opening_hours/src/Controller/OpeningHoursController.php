cd52962064ad20d4b7a9668aface6c7ce076f36f
Kasper Birch <kasperbirch1@gmail.com>
2024-03-14 20:24:24 +0100

    Update access permissions to render the `opening-hours-editor` app in editor
    This will prevent the tab and app from being displayed on content types other than branch

--- web/modules/custom/dpl_opening_hours/src/Controller/OpeningHoursController.php
+++ web/modules/custom/dpl_opening_hours/src/Controller/OpeningHoursController.php
@@ -2,14 +2,45 @@
 
 namespace Drupal\dpl_opening_hours\Controller;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Defines OpeningHoursController class.
+ */
 class OpeningHoursController extends ControllerBase {
 
+  /**
+   * Display the opening hours app.
+   */
   public function content() {
     return [
       '#theme' => 'dpl_react_app',
-      "#name" => 'opening-hours-editor',
+      '#name' => 'opening-hours-editor',
     ];
   }
+
+  /**
+   * Check access for a specific node.
+   *
+   * @param int $node
+   *   The node ID.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
+   */
+  public function adminTabAccess($node) {
+    $node_storage = $this->entityTypeManager()->getStorage('node');
+    $node_entity = $node_storage->load($node);
+
+    if ($node_entity) {
+      $node_type = $node_entity->getType();
+
+      if ($node_type == 'branch') {
+        return AccessResult::allowed();
+      }
+    }
+    return AccessResult::forbidden();
+  }
+
 }
