2e4bfa64842706ce8150d907839c5c21b0ed3986
clausbruun <claus@b-ruu.dk>
2024-04-16 15:26:30 +0200

    Adds User Registration Section linklist paragraph for users to insert on pages where shortcuts to User Registration items should be displayed.

---
+++ web/modules/custom/dpl_paragraphs/js/dpl_paragraphs.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Checks if a paragraph of type 'user_registration_linklist'
+ * exists in the DOM. If it does, it replaces it with a
+ * the 'user_registration_section__button_row' paragraph.
+ */
+(function (Drupal) {
+  Drupal.behaviors.dplParagraphsBehavior = {
+    attach(context, settings) {
+      const buttonRow = context.querySelector(
+        ".paragraphs__item--user_registration_section__button_row"
+      );
+      const linkList = context.querySelector(
+        ".paragraphs__item--user_registration_linklist"
+      );
+      if (buttonRow && linkList) {
+        if (buttonRow.children.length > 0) {
+          linkList.appendChild(buttonRow, linkList);
+          buttonRow.style.display = "block";
+        } else {
+          buttonRow.style.display = "block";
+        }
+      } else if (buttonRow) {
+        buttonRow.style.display = "block";
+      }
+    },
+  };
+})(Drupal);
