4dadaff92d3abe1aa0c6b4965391975b4f80c384
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-04-22 12:55:54 +0200

    Add support for repetitions of opening hours
    A new required repetition property is added to all opening hours
    instances.
    
    This has the following properties:
    
    1. id - for identifying instances belonging to the same repetition
    2. type - for specifying the type of repetition
    3. [type]_data - for additional data for the repetition type.
    
    We currently only support two repetition types:
    
    - None - which do not require addition data
    - Weekly - which only requires an end date

--- web/modules/custom/dpl_opening_hours/src/Plugin/rest/resource/OpeningHoursResourceBase.php
+++ web/modules/custom/dpl_opening_hours/src/Plugin/rest/resource/OpeningHoursResourceBase.php
@@ -10,6 +10,7 @@ use Drupal\rest\Plugin\ResourceBase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use function Safe\preg_replace as preg_replace;
 
 /**
  * Base class for REST resources exposing opening hours.
@@ -104,17 +105,64 @@ abstract class OpeningHoursResourceBase extends ResourceBase {
           "type" => "integer",
           "description" => "The id for the branch the instance belongs to",
         ],
+        "repetition" => [
+          "type" => "object",
+          "properties" => [
+            "id" => [
+              "type" => "integer",
+              "description" => $this->formatMultilineDescription(
+                "A serial unique id of the repetition. All instances with the same id belongs to the " .
+                "same repetition.",
+              ),
+            ],
+            "type" => [
+              "type" => "string",
+              "description" => $this->formatMultilineDescription(
+                "If/how the instance should be repeated in the future: \n" .
+                "  - single: The instance should not be repeated \n" .
+                "  - weekly: The instance should be repeated weekly from the first day of the repetition until the " .
+                "            provided end date. The week day of the first instance defines which weekday should be " .
+                "            used for the repeated instances."
+              ),
+              "enum" => [
+                "none",
+                "weekly",
+              ],
+            ],
+            // If a repetition type requires additional data then a
+            // corresponding property with the name "[repetition_type]_data"
+            // and the type "object" must be added. The properties of this
+            // object must contain all this data.
+            "weekly_data" => [
+              "type" => "object",
+              "properties" => [
+                "end_date" => [
+                  "type" => "string",
+                  "format" => "date",
+                  "description" => $this->formatMultilineDescription(
+                    "The end date of the repetition. If the end date is not on the same week day as the first " .
+                    "instance then the preceding occurrence of the weekday will be the last instance. \n\n" .
+                    "This field must be provided if type is 'weekly'",
+                  ),
+                ],
+              ],
+            ],
+          ],
+          "required" => [
+            ... ($require_id ? ["id"] : []),
+            "type",
+          ],
+        ],
       ],
-      "required" => array_merge(
-        ($require_id ? ["id"] : []),
-        [
-          "category",
-          "date",
-          "start_time",
-          "end_time",
-          "branch_id",
-        ]
-      ),
+      "required" => [
+        ... ($require_id ? ["id"] : []),
+        "category",
+        "date",
+        "start_time",
+        "end_time",
+        "branch_id",
+        "repetition",
+      ],
     ];
   }
 
@@ -159,6 +207,25 @@ abstract class OpeningHoursResourceBase extends ResourceBase {
       throw new \InvalidArgumentException("Unable to deserialize request");
     }
     return $requestData;
+  }
+
+  /**
+   * Format a multiline OpenAPI description.
+   *
+   * Multiline descriptions should:
+   *
+   * 1. Be readable in local PHP code
+   * 2. Render well with Swagger UI
+   * 3. Be parsable by openapitools/openapi-generator-cli
+   *
+   * This ensures that linebreaks (\n) placed by developers are converted to
+   * break tags for Swagger UI. Duplicate whitespaces added for local
+   * readability is also stripped.
+   */
+  private function formatMultilineDescription(string $description): string {
+    $no_newlines = preg_replace("/\n/", '<br/>', $description);
+    $no_extra_whitespace = preg_replace("/\s+/", " ", $no_newlines);
+    return $no_extra_whitespace;
   }
 
 }
