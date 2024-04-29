eb29736c9bc53ee1ed5de614d4d8679f36b5e103
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-04-30 01:13:23 +0200

    Add initial support for visual regression testing with Chromatic
    This is the result of following the official instructions for
    doing visual regression testing with Cypress and Chromatic:
    https://www.chromatic.com/docs/cypress/
    
    - Add Chromatic/Cypress packages
    - Install the plugin
    - Add required environment variable when running Cypress
    - Ignore the generated files

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+const { installPlugin } = require("@chromatic-com/cypress");
 
 module.exports = defineConfig({
   e2e: {
     // baseUrl is set using environment variables because it differs between
     // development and CI setups.
+    setupNodeEvents(on, config) {
+      installPlugin(on, config);
+    },
   },
   env: {
     // This is intentionally left empty.
