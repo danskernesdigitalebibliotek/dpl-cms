fdc81b5a8afde2a253c3583c5a31f763511ab1e4
Benjamin Rasmussen <benjamin@reload.dk>
2024-05-24 12:37:41 +0200

    Enable & setup config_ignore_auto module. DDFFORM-82
    We want site administrators to be able to add their own modules, along
    with their own settings, and this is not possible with Drupal
    configuration management.
    In the past, we used `config_ignore`, but this required a massive amount
    of work for developers, as all new configuration had to be added
    manually.
    `config_ignore_auto` works, by us enabling it on production sites, and
    then treating any config changes as something that cannot be overriden
    in the future by `drush cim`.
    See the future ADR for more documentation about pros, cons and effects.

--- assets/development.settings.php
+++ assets/development.settings.php
@@ -3,3 +3,5 @@
 # Enable verbose error reporting.
 $config['system.logging']['error_level'] = 'verbose';
 
+// Disable the "config auto ignore" module on local development.
+$config['config_ignore_auto.settings']['status'] = FALSE;
