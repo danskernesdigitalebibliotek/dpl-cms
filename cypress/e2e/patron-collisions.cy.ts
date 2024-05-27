634bce0585156655ea3bf4f5c5898c2c7d8b76c3
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-05-27 08:50:12 +0200

    Improve documentation for patron collision Cypress test

--- cypress/e2e/patron-collisions.cy.ts
+++ cypress/e2e/patron-collisions.cy.ts
@@ -1,4 +1,4 @@
-describe("Adgangsplatformen / CMS users", () => {
+describe("Adgangsplatformen / CMS user / session mapping", () => {
   const patron1 = {
     authorizationCode: "7c5e3213aea6ef42ec97dfeaa6f5b1d454d856db",
     accessToken: "patron1-token",
@@ -10,6 +10,7 @@ describe("Adgangsplatformen / CMS users", () => {
     // All values here are shifted by 1 digit.
     authorizationCode: "7c5e3213aea6ef42ec97dfeaa6f5b1d454d856dc",
     accessToken: "patron2-token",
+    // This patron has a CPR which partially overlaps with patron1.
     userCPR: 9999999999,
     userGuid: "19a4ae39-be07-4db9-a8b7-8bbb29f03da6",
   };
@@ -17,6 +18,7 @@ describe("Adgangsplatformen / CMS users", () => {
   const patron3 = {
     authorizationCode: "7c5e3213aea6ef42ec97dfeaa6f5b1d454d856dd",
     accessToken: "patron3-token",
+    // This patron has a CPR which is entirely different from patron 1.
     userCPR: 1111111111,
     userGuid: "19a4ae39-be07-4db9-a8b7-8bbb29f03da7",
   };
@@ -34,22 +36,27 @@ describe("Adgangsplatformen / CMS users", () => {
     cy.adgangsplatformenLogin(patron1);
     cy.verifyToken({ tokenType: "user", token: patron1.accessToken });
 
+    // Restoring the session should retain the access token.
+    // This is simulated by adgangsplatformenLogin() as this will restore the
+    // session for the patron when invoked with the same parameters.
     cy.adgangsplatformenLogin(patron1);
     cy.verifyToken({ tokenType: "user", token: patron1.accessToken });
   });
 
-  it("handles logins with overlapping idents", () => {
+  it("handles logins with overlapping CPRs", () => {
     cy.adgangsplatformenLogin(patron1);
     cy.verifyToken({ tokenType: "user", token: patron1.accessToken });
 
     cy.adgangsplatformenLogin(patron2);
     cy.verifyToken({ tokenType: "user", token: patron2.accessToken });
 
+    // When patron 1 continues with her session site she must retain her access
+    // token even though the CPRs are adjacent.
     cy.adgangsplatformenLogin(patron1);
     cy.verifyToken({ tokenType: "user", token: patron1.accessToken });
   });
 
-  it("handles logins with different idents", () => {
+  it("handles logins with different CPRs", () => {
     cy.adgangsplatformenLogin(patron1);
     cy.verifyToken({ tokenType: "user", token: patron1.accessToken });
 
