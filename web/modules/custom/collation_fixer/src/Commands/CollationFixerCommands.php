a3fdb6b1de493665cd83a7305628b22ec570bbc9
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-05-28 21:33:43 +0200

    Port/fix Drush commands

--- web/modules/custom/collation_fixer/src/Commands/CollationFixerCommands.php
+++ web/modules/custom/collation_fixer/src/Commands/CollationFixerCommands.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\collation_fixer\Drush\Commands;
+namespace Drupal\collation_fixer\Commands;
 
 use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Drupal\collation_fixer\CollationFixer;
@@ -22,29 +22,21 @@ final class CollationFixerCommands extends DrushCommands
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('collation_fixer.collation_fixer'),
-    );
-  }
-
-  /**
    * Command description here.
+   *
+   * @command collation-fixer
+   * @option table Name of a table to fix collation on. Defaults to all tables
    */
-  #[CLI\Command(name: 'collation-fixer')]
-  #[CLI\Option(name: 'table', description: 'Name of a table to fix collation on. Defaults to all tables')]
-  public function commandName($options = ['table' => NULL]) {
+  public function fixTable($options = ['table' => NULL]) {
     $tables = $this->collationFixer->checkCollation($options['table']);
     $total = array_map(function (string $table) {
       return $this->collationFixer->fixCollation($table);
     }, $tables);
 
-    if ($total > 0) {
-      $this->logger->log('@current tables  were processed.', ['@current' => count($total)]);
+    if (count($total)) {
+      $this->io()->success(sprintf('%s tables were processed.', count($total)));
     } else {
-      $this->logger->log('No collation fixes needs to be done');
+      $this->io()->info('No collation fixes needs to be done');
     }
   }
 
