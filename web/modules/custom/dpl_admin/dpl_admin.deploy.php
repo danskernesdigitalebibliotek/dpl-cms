9ffc05a70f9e6aea82e4ce3c8f757aeb4ab22cc4
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-06-05 15:10:53 +0200

    Move deploy hooks for updating sorting to update module
    They are not directly related to the admin module so move it to the
    more generic module.

--- web/modules/custom/dpl_admin/dpl_admin.deploy.php
+++ web/modules/custom/dpl_admin/dpl_admin.deploy.php
@@ -7,10 +7,6 @@
  * These get run AFTER config-import.
  */
 
-use Drupal\collation_fixer\CollationFixer;
-use Drupal\drupal_typed\DrupalTyped;
-use Drupal\node\NodeInterface;
-
 /**
  * We've limited the text formats for CKEditor fields. Let's move old to new.
  */
@@ -68,36 +64,4 @@ function dpl_admin_deploy_set_config_auto(): string {
   $config->save(TRUE);
 
   return 'config_ignore_auto.settings.ignored_config_entities has been reset.';
-}
-
-/**
- * Fix collation for all tables to fix alphabetical sorting.
- */
-function dpl_admin_deploy_fix_collation(): string {
-  if (!\Drupal::moduleHandler()->moduleExists('collation_fixer')) {
-    return "No table collations fixed. collation_fixer module is not enabled.";
-  }
-  $collation_fixer = DrupalTyped::service(CollationFixer::class, CollationFixer::class);
-  $collation_fixer->fixCollation();
-  return "Fixed collation for all tables";
-}
-
-/**
- * Set branches without value to not promoted on lists.
- */
-function dpl_admin_deploy_set_branches_not_promoted(): string {
-  $branches = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(
-    ['type' => 'branch'],
-  );
-  $branches_with_empty_promotion_fields = array_filter(
-    $branches,
-    fn(NodeInterface $branch) => $branch->get('field_promoted_on_lists')->isEmpty()
-  );
-  array_map(
-    fn(NodeInterface $branch) => $branch->set('field_promoted_on_lists', 0)->save(),
-    $branches_with_empty_promotion_fields
-  );
-
-  $count_branches = count($branches_with_empty_promotion_fields);
-  return "Set default value for promoted on lists for {$count_branches} branches";
 }
