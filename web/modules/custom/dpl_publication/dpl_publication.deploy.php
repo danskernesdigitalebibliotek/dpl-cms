e84cd2b28d8590706cccd9a406f687ca6764b6a2
Kasper Birch <kasperbirch1@gmail.com>
2024-06-12 20:45:12 +0200

    Optimize `dpl_publication_deploy_default_publication_date` query + logging
    - Updated the query to only fetch relevant nodes (published and without a value in `field_publication_date`).
    - Utilized `Node::loadMultiple` for batch loading nodes.
    - Returned a string with relevant information to be shown in the terminal.

--- web/modules/custom/dpl_publication/dpl_publication.deploy.php
+++ web/modules/custom/dpl_publication/dpl_publication.deploy.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * @file
- * Fix this.
- *
- * @todo Fix this todo.
- *
- * This does not run with task dev:reset.
- * It only works if I change the hook name and execute dec drush deploy:hook.
- */
-
 use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\node\Entity\Node;
 
@@ -17,31 +7,34 @@ use Drupal\node\Entity\Node;
  * Implements hook_deploy().
  *
  * Deploy hook to set publication date for all existing nodes of
- *  type "article" And "page".
+ * type "article" and "page".
  */
-function dpl_publication_deploy_default_publication_date(): void {
+function dpl_publication_deploy_default_publication_date(): string {
   $nids = Drupal::entityQuery('node')
     ->condition('type', ['article', 'page'], 'IN')
+    ->condition('status', 1)
+    ->notExists('field_publication_date')
     ->accessCheck(TRUE)
     ->execute();
 
-  // Load each node and update the publication date.
-  foreach ($nids as $nid) {
-    $node = Node::load($nid);
-
-    // Check if the node is published and does not have a publication date set.
-    if ($node && $node->isPublished() && !$node->get('field_publication_date')
-      ->getValue()) {
-      // Get the creation time.
-      $creation_time = $node->getCreatedTime();
-
-      // Convert the creation time to the 'Y-m-d' format.
-      $creation_date = DrupalDateTime::createFromTimestamp($creation_time)
-        ->format('Y-m-d');
-
-      // Set the field_publication_date to the creation date.
-      $node->set('field_publication_date', $creation_date);
-      $node->save();
-    }
+  if (empty($nids)) {
+    return 'No nodes found to update.';
   }
+
+  $nodes = Node::loadMultiple($nids);
+
+  $node_labels = [];
+  foreach ($nodes as $node) {
+    $creation_time = $node->getCreatedTime();
+
+    $creation_date = DrupalDateTime::createFromTimestamp($creation_time)
+      ->format('Y-m-d');
+
+    $node->set('field_publication_date', $creation_date);
+    $node->save();
+
+    $node_labels[] = $node->label();
+  }
+
+  return 'Updated nodes: ' . implode(', ', $node_labels);
 }
