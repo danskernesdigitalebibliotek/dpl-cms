ecd447b210ec5a5f08ba89e13fea08b2f8a1fe98
Benjamin Rasmussen <benjamin@reload.dk>
2024-06-18 12:45:55 +0200

    EventAPI: Replace Annotation with PHP code.
    Up until now, we've defined the API using @rest code comments, but this
    is really hard to manage and debug.
    The commit changes it to a classic PHP array.

--- web/modules/custom/dpl_event/src/Plugin/rest/resource/v1/EventResource.php
+++ web/modules/custom/dpl_event/src/Plugin/rest/resource/v1/EventResource.php
@@ -4,14 +4,12 @@ namespace Drupal\dpl_event\Plugin\rest\resource\v1;
 
 use DanskernesDigitaleBibliotek\CMS\Api\Model\EventPATCHRequest;
 use DanskernesDigitaleBibliotek\CMS\Api\Model\EventPATCHRequestExternalData;
+use Drupal\Component\Utility\NestedArray;
 use Drupal\recurring_events\Entity\EventInstance;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-// Descriptions quickly become long and Doctrine annotations have no good way
-// of handling multiline strings.
-// phpcs:disable Drupal.Files.LineLength.TooLong
 /**
  * REST resource for working with single events.
  *
@@ -20,80 +18,86 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  *   label = @Translation("Update single events"),
  *   uri_paths = {
  *     "canonical" = "/api/v1/events/{uuid}",
- *   },
- *
- *   route_parameters = {
- *     "GET" = {
- *       "uuid" = {
- *         "name" = "uuid",
- *         "description" = "The unique identifier of the event to update. Use the same value as provided for the event in the event list.",
- *         "type" = "string",
- *         "in" = "query",
- *         "required" = TRUE,
- *       },
- *     },
- *   },
- *
- *   payload = {
- *     "name" = "event",
- *     "description" = "Data to update the event with.",
- *     "in" = "body",
- *     "required" = TRUE,
- *     "schema" = {
- *       "type" = "object",
- *       "properties" = {
- *         "state" = {
- *           "type" = "string",
- *           "description" = "The state of the event.",
- *           "enum" = {
- *             "TicketSaleNotOpen",
- *             "Active",
- *             "SoldOut",
- *             "Cancelled",
- *             "Occurred",
- *           },
- *         },
- *         "external_data" = {
- *           "type" = "object",
- *           "description" = "Data for the event provided by a third party.",
- *           "properties" = {
- *             "url" = {
- *               "type" = "string",
- *               "format" = "uri",
- *               "description" = "An absolute url provided by the third party where end users can access the event.",
- *             },
- *             "admin_url" = {
- *               "type" = "string",
- *               "format" = "uri",
- *               "description" = "An absolute url provided by the third party where editorial users can administer the event. Accessing this url should require authentication.",
- *             },
- *           },
- *         },
- *       },
- *     },
- *   },
- *
- *   responses = {
- *     200 = {
- *       "description" = "OK",
- *     },
- *     400 = {
- *      "description" = "Invalid input format"
- *     },
- *     403 = {
- *      "description" = "Access denied"
- *     },
- *     404 = {
- *       "description" = "Event not found"
- *     },
- *     500 = {
- *       "description" = "Internal server error"
- *     },
  *   }
  * )
  */
 final class EventResource extends EventResourceBase {
-// phpcs:enable Drupal.Files.LineLength.TooLong
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getPluginDefinition(): array {
+    return NestedArray::mergeDeep(
+      parent::getPluginDefinition(),
+      [
+        'route_parameters' => [
+          'GET' => [
+            'uuid' => [
+              'name' => 'uuid',
+              'description' => 'The unique identifier of the event to update. Use the same value as provided for the event in the event list.',
+              'type' => 'string',
+              'in' => 'query',
+              'required' => TRUE,
+            ],
+          ],
+        ],
+        'payload' => [
+          'name' => 'event',
+          'description' => 'Data to update the event with.',
+          'in' => 'body',
+          'required' => TRUE,
+          'schema' => [
+            'type' => 'object',
+            'properties' => [
+              'state' => [
+                'type' => 'string',
+                'description' => 'The state of the event.',
+                'enum' => [
+                  'TicketSaleNotOpen',
+                  'Active',
+                  'SoldOut',
+                  'Cancelled',
+                  'Occurred',
+                ],
+              ],
+              'external_data' => [
+                'type' => 'object',
+                'description' => 'Data for the event provided by a third party.',
+                'properties' => [
+                  'url' => [
+                    'type' => 'string',
+                    'format' => 'uri',
+                    'description' => 'An absolute URL provided by the third party where end users can access the event.',
+                  ],
+                  'admin_url' => [
+                    'type' => 'string',
+                    'format' => 'uri',
+                    'description' => 'An absolute URL provided by the third party where editorial users can administer the event. Accessing this URL should require authentication.',
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ],
+        'responses' => [
+          200 => [
+            'description' => 'OK',
+          ],
+          400 => [
+            'description' => 'Invalid input format',
+          ],
+          403 => [
+            'description' => 'Access denied',
+          ],
+          404 => [
+            'description' => 'Event not found',
+          ],
+          500 => [
+            'description' => 'Internal server error',
+          ],
+        ],
+      ]);
+  }
 
   /**
    * PATCH requests - Load the relevant eventinstance, and update values.
