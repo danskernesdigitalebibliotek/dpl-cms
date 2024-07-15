f14ddd36e06b6d167f86382705070ca51ed0769d
Kasper Garnæs <kasperg@users.noreply.github.com>
2024-07-15 13:37:52 +0200

    Remove comment about number of elements needed for grid
    Practice shows that 3 are not needed and now the minimum is 1 anyway.

--- web/modules/custom/dpl_related_content/src/Services/RelatedContent.php
+++ web/modules/custom/dpl_related_content/src/Services/RelatedContent.php
@@ -547,8 +547,6 @@ class RelatedContent {
     }
 
     if ($this->listStyle == RelatedContentListStyle::Grid) {
-      // Visually, grid looks broken with less than 3 items, and does not
-      // support more than 6.
       $this->minItems = 1;
       $this->maxItems = 6;
     }
