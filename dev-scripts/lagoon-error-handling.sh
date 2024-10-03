fbd530d045633256c983a4f5f1b4cc5974154626
Benjamin Rasmussen <rasben@users.noreply.github.com>
2024-10-03 13:18:52 +0200

    Include lagoon log URL in GH deployments.

--- dev-scripts/lagoon-error-handling.sh
+++ dev-scripts/lagoon-error-handling.sh
@@ -6,13 +6,14 @@
 error_handler() {
   DEPLOYMENT_STATUS="failure"
   GH_DEPLOYMENT_ID=$(cat /tmp/gh_deployment_id)
+  LAGOON_DEPLOYS_LOG_URL=$(cat /tmp/LAGOON_DEPLOYS_LOG_URL)
   echo "Setting GH deployment status '$GH_DEPLOYMENT_ID': '$DEPLOYMENT_STATUS'"
 
   curl -L \
     -X POST \
     -H "Authorization: Bearer $GH_DEPLOYMENT_TOKEN" \
     https://api.github.com/repos/danskernesdigitalebibliotek/dpl-cms/deployments/"$GH_DEPLOYMENT_ID"/statuses \
-    -d "{\"environment\":\"$LAGOON_PR_HEAD_BRANCH\",\"state\":\"$DEPLOYMENT_STATUS\"}"
+    -d "{\"environment\":\"$LAGOON_ENVIRONMENT\",\"log_url\":\"$LAGOON_DEPLOYS_LOG_URL\",\"state\":\"$DEPLOYMENT_STATUS\"}"
 
   # As we've gotten rid of set -e in lagoon.yml, it is very important to exit
   # here, otherwise, lagoon will think a faulty deploy has succeeded.
