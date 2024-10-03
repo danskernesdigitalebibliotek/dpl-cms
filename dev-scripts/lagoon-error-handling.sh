5b2991df5913756677520dd0c9c3af6a9bc01f01
Benjamin Rasmussen <rasben@users.noreply.github.com>
2024-10-03 13:59:20 +0200

    Creating the GH deployment through action, finding ID in lagoon.yml

--- dev-scripts/lagoon-error-handling.sh
+++ dev-scripts/lagoon-error-handling.sh
@@ -4,16 +4,7 @@
 # along the failing details to the GH deployment.
 
 error_handler() {
-  DEPLOYMENT_STATUS="failure"
-  GH_DEPLOYMENT_ID=$(cat /tmp/gh_deployment_id)
-  LAGOON_DEPLOYS_LOG_URL=$(cat /tmp/LAGOON_DEPLOYS_LOG_URL)
-  echo "Setting GH deployment status '$GH_DEPLOYMENT_ID': '$DEPLOYMENT_STATUS'"
-
-  curl -L \
-    -X POST \
-    -H "Authorization: Bearer $GH_DEPLOYMENT_TOKEN" \
-    https://api.github.com/repos/danskernesdigitalebibliotek/dpl-cms/deployments/"$GH_DEPLOYMENT_ID"/statuses \
-    -d "{\"environment\":\"$LAGOON_ENVIRONMENT\",\"log_url\":\"$LAGOON_DEPLOYS_LOG_URL\",\"state\":\"$DEPLOYMENT_STATUS\"}"
+  ./dev-scripts/lagoon-set-gh-deploy-status.sh "failure"
 
   # As we've gotten rid of set -e in lagoon.yml, it is very important to exit
   # here, otherwise, lagoon will think a faulty deploy has succeeded.
