3006da03137a95afe86493bf17fca16d620e1bf6
Kasper Birch <kasperbirch1@gmail.com>
2024-10-30 13:52:58 +0100

    Test media library / `Media(s)`paragraph
    This was also a bit tricky, but I found some inspiration in this library that I rewrote: https://github.com/kanopi/shrubs/blob/main/mediaLibrarySelect.js

--- cypress/e2e/paragraphs.cy.ts
+++ cypress/e2e/paragraphs.cy.ts
@@ -31,6 +31,48 @@ const typeInCkEditor = (content: string) => {
   });
 };
 
+// INSPRIATION: https://github.com/kanopi/shrubs/blob/main/mediaLibrarySelect.js
+const mediaLibrarySelect = (fileName: string, id: number) => {
+  // Need to create unique intercepts for each media library select
+  const mediaNodeEditAjax = `mediaNodeEditAjax${id}`;
+  const mediaLibraryAjax = `mediaLibraryAjax${id}`;
+  const viewsAjax = `viewsAjax${id}`;
+
+  cy.intercept("POST", "/node/*/**").as(mediaNodeEditAjax);
+  cy.intercept("POST", "/media-library**").as(mediaLibraryAjax);
+  cy.intercept("GET", "/views/ajax?**").as(viewsAjax);
+
+  cy.get(
+    "#field_medias-media-library-wrapper-field_paragraphs-0-subform"
+  ).within(() => {
+    cy.get('input[value="Add media"]').click();
+  });
+
+  cy.wait(`@${mediaNodeEditAjax}`).its("response.statusCode").should("eq", 200);
+
+  cy.get(".media-library-widget-modal").within(() => {
+    cy.get('.views-exposed-form input[name="search"]').clear().type(fileName);
+    cy.get('.views-exposed-form input[type="submit"]').click();
+    cy.wait(`@${viewsAjax}`, { timeout: 10000 })
+      .its("response.statusCode")
+      .should("eq", 200);
+    cy.get(".media-library-views-form .views-row").first().click();
+
+    cy.get(".form-actions button").contains("Insert selected").click();
+  });
+
+  cy.wait(`@${mediaNodeEditAjax}`).its("response.statusCode").should("eq", 200);
+};
+
+type CheckImageSrcType = {
+  selector: string;
+  expectedInSrc: string;
+};
+
+const checkImageSrc = ({ selector, expectedInSrc }: CheckImageSrcType) => {
+  cy.get(selector).should("have.attr", "src").should("include", expectedInSrc);
+};
+
 describe("Paragraph module", () => {
   beforeEach(() => {
     cy.deleteAllContentIfExists(pageName, "page");
@@ -45,5 +87,39 @@ describe("Paragraph module", () => {
     cy.saveContent();
 
     cy.get(".rich-text").should("contain", "Hello, world!");
+  });
+
+  it("Can add 'media'", () => {
+    addParagraph("Media(s)");
+
+    mediaLibrarySelect("Læseklubber", 1);
+
+    cy.saveContent();
+
+    checkImageSrc({
+      selector: ".medias.medias--single img",
+      expectedInSrc: "laeseklubber.jpg",
+    });
+  });
+
+  it("Can add 'media' (2 images)", () => {
+    addParagraph("Media(s)");
+    const images = ["Læseklubber", "robert-collins"];
+
+    images.forEach((img, index) => {
+      mediaLibrarySelect(img, index);
+    });
+
+    cy.saveContent();
+
+    checkImageSrc({
+      selector: ".medias__item.medias__item--first img",
+      expectedInSrc: "laeseklubber.jpg",
+    });
+
+    checkImageSrc({
+      selector: ".medias__item.medias__item--last img",
+      expectedInSrc: "robert-collins-tvc5imO5pXk-unsplash_0.jpg",
+    });
   });
 });
