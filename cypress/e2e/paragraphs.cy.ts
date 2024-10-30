5b9e3eab3db68b1fcc13afada97c9b963fed0376
Kasper Birch <kasperbirch1@gmail.com>
2024-10-30 13:49:42 +0100

    Test ckeditor / `Text body` paragraph
    This was tricky. I tried installing the npm package `Cypress Real Events` and that works. But I wonder why I couldn’t use `cy.realClick` instead of using `.click({ multiple: true, force: true, })`. However, I ended up writing my own `typeInCkEditor` with inspiration from this: https://drupal.stackexchange.com/questions/315635/how-to-write-in-a-ckdeditor-textarea-with-cypress

--- cypress/e2e/paragraphs.cy.ts
+++ cypress/e2e/paragraphs.cy.ts
@@ -5,9 +5,45 @@ const createTestPageAndOpenParagraphModal = () => {
   cy.findByLabelText("Title").type(pageName);
   cy.openParagraphsModal();
 };
+
+const addParagraph = (paragraphType: string) => {
+  cy.get(`button[value="${paragraphType}"]`).click({
+    multiple: true,
+    force: true,
+  });
+};
+
+// INSPRIATION: https://drupal.stackexchange.com/questions/315635/how-to-write-in-a-ckdeditor-textarea-with-cypress
+const typeInCkEditor = (content: string) => {
+  // Ensure the CKEditor is visible
+  cy.get(".ck-editor__editable").should("be.visible");
+
+  cy.window().then((win) => {
+    // @ts-expect-error - Drupal may not exist on the window object
+    if (win.Drupal && win.Drupal.CKEditor5Instances) {
+      // @ts-expect-error - CKEditor5Instances may not match expected structure
+      win.Drupal.CKEditor5Instances.forEach(
+        (editor: { setData: (content: string) => void }) => {
+          editor.setData(content);
+        }
+      );
+    }
+  });
+};
+
 describe("Paragraph module", () => {
   beforeEach(() => {
     cy.deleteAllContentIfExists(pageName, "page");
     createTestPageAndOpenParagraphModal();
+  });
+
+  it("Can add 'Text body' (CEK editor)", () => {
+    addParagraph("Text body");
+
+    typeInCkEditor("Hello, world!");
+
+    cy.saveContent();
+
+    cy.get(".rich-text").should("contain", "Hello, world!");
   });
 });
