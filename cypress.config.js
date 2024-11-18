8d1a4b259bd5c507ce2e2b71c729cbdcdb97bdf6
Kasper Birch <kasperbirch1@gmail.com>
2024-11-18 09:49:49 +0100

    Enable retries for flaky Cypress tests
    To mitigate failures caused by inconsistent load times in React apps and Drupal, retries have been added to stabilize test outcomes.

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
     // baseUrl is set using environment variables because it differs between
     // development and CI setups.
     defaultCommandTimeout: 10000,
+    retries: {
+      runMode: 3,
+      openMode: 0,
+    },
   },
   env: {
     // This is intentionally left empty.
