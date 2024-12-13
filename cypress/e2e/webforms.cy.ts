dcd110461e7701a5f217ef9797acc13012518e5f
Andreas Nielsen <andreas@reload.dk>
2024-12-13 16:38:44 +0100

    Added 3 new Cypress tests:  - Add new webform and add a new element to it.  - Add a new email handler to an existing webform.  - Delete an existing webform.
    The 2 first tests could maybe be expanded upon if needed e.g adding
    more types of elements or adding more complext email handlers etc.
    
    But for now this will do.
    
    I'm not sure that all of the cypress coding standards has been followed,
    but I have found it difficult to select specific elements in the Drupal
    structured DOM.

--- cypress/e2e/webforms.cy.ts
+++ cypress/e2e/webforms.cy.ts
@@ -24,7 +24,10 @@ describe('Webforms', () => {
     cy.visit('/kontakt');
     cy.findByRole('button', { name: 'Send besked' }).click();
     cy.findByLabelText('Dit navn').then(($input) => {
-      expect($input[0].validationMessage).to.eq('Please fill in this field.');
+      expect($input[0].validationMessage).to.be.oneOf([
+        'Please fill in this field.',
+        'Please fill out this field.',
+      ]);
     });
   });
 
@@ -68,6 +71,64 @@ describe('Webforms', () => {
     cy.findByRole('button', { name: 'Send besked' }).click();
     cy.get('.error-message__description').contains(
       'There was a problem with your form submission.',
+    );
+  });
+
+  it('Create a new webform with textfield element.', () => {
+    cy.drupalLogin('/admin/structure/webform/add');
+    cy.findByLabelText('Title').type('Cypress Test Webform');
+    cy.findByRole('button', { name: 'Save' }).click();
+    cy.url().should(
+      'match',
+      /admin\/structure\/webform\/manage\/cypress_test_webform/,
+    );
+    cy.get('a#webform-ui-add-element').click();
+    cy.get(
+      'a[data-drupal-selector="edit-elements-textfield-operation"]',
+    ).click();
+    //cy.findByLabelText('Title').closest('input').type('Test text field');
+    cy.get('input[data-drupal-selector="title"]').type('Test text field');
+    cy.findByRole('button', { name: 'Save' }).click();
+  });
+
+  it('Can add a new email handler to an existing webform.', () => {
+    cy.drupalLogin('/admin/structure/webform');
+    cy.get('td')
+      .contains('Cypress Test Webform')
+      .closest('tr')
+      .contains('li > a', 'Build')
+      .click();
+    cy.get('.tabs__link').contains('Settings').click();
+    cy.get('.tabs__link').contains('Emails / Handlers').click();
+    cy.get('.local-actions__item').contains('Add email').click();
+    cy.findByLabelText('Title').clear();
+    cy.findByLabelText('Title').type('Cypress Test Email Handlers');
+    cy.findByRole('button', { name: 'Save' }).click();
+  });
+
+  it('Can delete an existing webform.', () => {
+    cy.drupalLogin('/admin/structure/webform');
+    cy.get('td')
+      .contains('Cypress Test Webform')
+      .closest('tr')
+      .findByRole('button')
+      .click()
+      .then(() => {
+        cy.get('td')
+          .contains('Cypress Test Webform')
+          .closest('tr')
+          .contains('li > a', 'Delete')
+          .click();
+      });
+    cy.get('.messages__content').contains(
+      'Are you sure you want to delete the Cypress Test Webform webform?',
+    );
+    cy.findByLabelText(
+      'Yes, I want to delete the Cypress Test Webform webform',
+    ).click();
+    cy.findByRole('button', { name: 'Delete' }).click();
+    cy.get('.messages__content').contains(
+      'The webform Cypress Test Webform has been deleted.',
     );
   });
 });
