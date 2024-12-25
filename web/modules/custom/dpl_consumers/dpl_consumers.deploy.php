d209277aef959e0ef476b5346c9163539df2623f
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-12-25 21:58:32 +0100

    Delete old consumer and add new ones in new deploy hook

--- web/modules/custom/dpl_consumers/dpl_consumers.deploy.php
+++ web/modules/custom/dpl_consumers/dpl_consumers.deploy.php
@@ -7,6 +7,10 @@
 
 declare(strict_types=1);
 
+use Drupal\dpl_consumers\Consumer;
+use Drupal\dpl_consumers\ConsumerRole;
+use Drupal\dpl_consumers\ConsumerUser;
+
 /**
  * Run consumer creation on deploy.
  *
@@ -20,8 +24,55 @@ declare(strict_types=1);
  * as we don't want to create duplicates.
  */
 function dpl_consumers_deploy_10001(): void {
-  dpl_consumers_delete_user();
-  dpl_consumers_delete_consumer();
-  dpl_consumers_create_user();
-  dpl_consumers_create_consumer();
+  // Noop. We don't need to do anything here.
+  // Since we have changed our minds.
+  // @see dpl_consumers_deploy_10002().
+}
+
+/**
+ * Expand the users, roles, consumers so they can handle both BNF and GO.
+ *
+ * So we delete previous entities and create new ones.
+ */
+function dpl_consumers_deploy_10002(): void {
+  // Delete consume and users that we want to handle differently.
+  // We want to create consumers and consumer users
+  // specifically for the two known consumers (BNF and Go) and connected users.
+  Consumer::deleteByClientId("graphql_consumer");
+  (new ConsumerUser('GraphQL Consumer', ''))->delete();
+  (new ConsumerUser('graphql_consumer', ''))->delete();
+
+  // Check if we have the necessary secrets for new consumers and their users.
+  if (!$bnf_consumer_secret = getenv('BNF_GRAPHQL_CONSUMER_SECRET')) {
+    throw new \Exception('BNF_GRAPHQL_CONSUMER_SECRET not found.');
+  }
+  if (!$go_consumer_secret = getenv('GO_GRAPHQL_CONSUMER_SECRET')) {
+    throw new \Exception('GO_GRAPHQL_CONSUMER_SECRET not found.');
+  }
+  if (!$bnf_consumer_user_password = getenv('BNF_GRAPHQL_CONSUMER_USER_PASSWORD')) {
+    throw new \Exception('BNF_GRAPHQL_CONSUMER_USER_PASSWORD not found.');
+  }
+  if (!$go_consumer_user_password = getenv('GO_GRAPHQL_CONSUMER_USER_PASSWORD')) {
+    throw new \Exception('GO_GRAPHQL_CONSUMER_USER_PASSWORD not found.');
+  }
+
+  // Create new consumers (BNF and Go) and their users and roles.
+  /** @var \Drupal\dpl_consumers\Services\ConsumerHandler $handler */
+  $consumer_handler = \Drupal::service('dpl_consumers.consumer_handler');
+  $consumers = [
+    [
+      'consumer' => new Consumer('BNF GraphQL', 'bnf_graphql', 'bnf_graphql', $bnf_consumer_secret),
+      'user' => new ConsumerUser('bnf_graphql', $bnf_consumer_user_password),
+      'role' => new ConsumerRole('bnf_graphql_client'),
+    ],
+    [
+      'consumer' => new Consumer('GO GraphQL', 'go_graphql', 'go_graphql', $go_consumer_secret),
+      'user' => new ConsumerUser('go_graphql', $go_consumer_user_password),
+      'role' => new ConsumerRole('go_graphql_client'),
+    ],
+  ];
+
+  foreach ($consumers as $consumer) {
+    $consumer_handler->setComponents($consumer['consumer'], $consumer['user'], $consumer['role'])->create();
+  }
 }
