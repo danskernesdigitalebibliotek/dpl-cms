ccd9cf96605f9d212537f53c6c5dd1370ab432ab
Andreas Nielsen <andreas@reload.dk>
2025-01-21 14:41:44 +0100

    Added two new GO paragraphs:
    - GO Material Slider - Automatic
    - GO Material Slider - Manual
    
    Enabled both of them for all GO specific content types (GO Article, GO Category, GO Page)
    
    Enabled both paragraphs in GraphQL Compose.
    
    Also enabled GO Linkbox paragraph for all GO specific content types and enabled it in GraphQL Compose.

---
+++ web/modules/custom/dpl_graphql/src/Plugin/GraphQLCompose/FieldType/WorkIdItem.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dpl_graphql\Plugin\GraphQLCompose\FieldType;
+
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\graphql\GraphQL\Execution\FieldContext;
+use Drupal\graphql_compose\Plugin\GraphQL\DataProducer\FieldProducerItemInterface;
+use Drupal\graphql_compose\Plugin\GraphQL\DataProducer\FieldProducerTrait;
+use Drupal\graphql_compose\Plugin\GraphQLCompose\GraphQLComposeFieldTypeBase;
+
+/**
+ * {@inheritDoc}
+ *
+ * @GraphQLComposeFieldType(
+ *   id = "dpl_fbi_work_id",
+ *   type_sdl = "WorkId",
+ * )
+ */
+class WorkIdItem extends GraphQLComposeFieldTypeBase implements FieldProducerItemInterface {
+
+  use FieldProducerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolveFieldItem(FieldItemInterface $item, FieldContext $context) {
+
+    return [
+      'value' => $item->value ?? NULL,
+      'material_type' => $item->material_type ?? NULL,
+    ];
+  }
+
+}
---
+++ web/modules/custom/dpl_graphql/src/Plugin/GraphQLCompose/SchemaType/WorkIdType.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dpl_graphql\Plugin\GraphQLCompose\SchemaType;
+
+use Drupal\graphql_compose\Plugin\GraphQLCompose\GraphQLComposeSchemaTypeBase;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+/**
+ * {@inheritdoc}
+ *
+ * @GraphQLComposeSchemaType(
+ *   id = "WorkId",
+ * )
+ */
+class WorkIdType extends GraphQLComposeSchemaTypeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTypes(): array {
+
+    $types = [];
+
+    $types[] = new ObjectType([
+      'name' => $this->getPluginId(),
+      'description' => (string) $this->t('A WorkID field.'),
+      'fields' => fn() => [
+        'value' => [
+          'type' => Type::string(),
+          'description' => (string) $this->t('The WorkID value'),
+        ],
+        'material_type' => [
+          'type' => Type::string(),
+          'description' => (string) $this->t('The material type (e.g., bog, e-bog).'),
+        ],
+      ],
+    ]);
+
+    return $types;
+  }
+
+}
