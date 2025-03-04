6d5956bd2ff96b8b565fa7503754c02e3a7bdb89
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-03-04 16:25:17 +0100

    Update PHPunit tests to match the new BNF reality. DDFHER-209

--- web/modules/custom/bnf/src/GraphQL/Operations/GetNode/Node/Paragraphs/Medias/MediaImage/Image.php
+++ web/modules/custom/bnf/src/GraphQL/Operations/GetNode/Node/Paragraphs/Medias/MediaImage/Image.php
@@ -5,20 +5,20 @@ namespace Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\Medias\MediaImag
 /**
  * @property string $url
  * @property string $__typename
- * @property string|null $alt
  * @property string|null $title
+ * @property string|null $alt
  */
 class Image extends \Spawnia\Sailor\ObjectLike
 {
     /**
      * @param string $url
-     * @param string|null $alt
      * @param string|null $title
+     * @param string|null $alt
      */
     public static function make(
         $url,
-        $alt = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
         $title = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
+        $alt = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
     ): self {
         $instance = new self;
 
@@ -26,11 +26,11 @@ class Image extends \Spawnia\Sailor\ObjectLike
             $instance->url = $url;
         }
         $instance->__typename = 'Image';
-        if ($alt !== self::UNDEFINED) {
-            $instance->alt = $alt;
-        }
         if ($title !== self::UNDEFINED) {
             $instance->title = $title;
+        }
+        if ($alt !== self::UNDEFINED) {
+            $instance->alt = $alt;
         }
 
         return $instance;
@@ -43,8 +43,8 @@ class Image extends \Spawnia\Sailor\ObjectLike
         return $converters ??= [
             'url' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Spawnia\Sailor\Convert\StringConverter),
             '__typename' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Spawnia\Sailor\Convert\StringConverter),
-            'alt' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\StringConverter),
             'title' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\StringConverter),
+            'alt' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\StringConverter),
         ];
     }
 
--- web/modules/custom/bnf/src/GraphQL/Operations/GetNode/Node/Paragraphs/SingleLink/Link.php
+++ web/modules/custom/bnf/src/GraphQL/Operations/GetNode/Node/Paragraphs/SingleLink/Link.php
@@ -5,20 +5,20 @@ namespace Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\SingleLink;
 /**
  * @property bool $internal
  * @property string $__typename
- * @property string|null $url
  * @property string|null $title
+ * @property string|null $url
  */
 class Link extends \Spawnia\Sailor\ObjectLike
 {
     /**
      * @param bool $internal
-     * @param string|null $url
      * @param string|null $title
+     * @param string|null $url
      */
     public static function make(
         $internal,
-        $url = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
         $title = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
+        $url = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
     ): self {
         $instance = new self;
 
@@ -26,11 +26,11 @@ class Link extends \Spawnia\Sailor\ObjectLike
             $instance->internal = $internal;
         }
         $instance->__typename = 'Link';
-        if ($url !== self::UNDEFINED) {
-            $instance->url = $url;
-        }
         if ($title !== self::UNDEFINED) {
             $instance->title = $title;
+        }
+        if ($url !== self::UNDEFINED) {
+            $instance->url = $url;
         }
 
         return $instance;
@@ -43,8 +43,8 @@ class Link extends \Spawnia\Sailor\ObjectLike
         return $converters ??= [
             'internal' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Spawnia\Sailor\Convert\BooleanConverter),
             '__typename' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Spawnia\Sailor\Convert\StringConverter),
-            'url' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\StringConverter),
             'title' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\StringConverter),
+            'url' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\StringConverter),
         ];
     }
 
