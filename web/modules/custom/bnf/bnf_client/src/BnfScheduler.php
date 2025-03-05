f0fba731f55164064d2669c770eed58c22d90faa
Thomas Fini Hansen <xen@xen.dk>
2025-03-05 10:43:04 +0100

    Add node updates and move to service
    Ref DDFHER-197

---
+++ web/modules/custom/bnf/bnf_client/src/BnfScheduler.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\bnf_client;
+
+use Drupal\bnf\BnfStateEnum;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Queue\QueueFactory;
+
+/**
+ * Handles queueing of updates to subscriptions and content.
+ */
+class BnfScheduler {
+
+  /**
+   * Constructor.
+   */
+  public function __construct(
+    protected EntityTypeManagerInterface $entityTypeManager,
+    protected QueueFactory $queueFactory,
+  ) {}
+
+  /**
+   * Queue an update check on all subscriptions.
+   */
+  public function queueAllSubscriptionsUpdate(): void {
+    /** @var \Drupal\bnf_client\Entity\Subscription[] $subscriptions */
+    $subscriptions = $this->entityTypeManager->getStorage('bnf_subscription')->loadMultiple();
+
+    $queue = $this->queueFactory->get('bnf_client_new_content');
+
+    foreach ($subscriptions as $subscription) {
+      $queue->createItem(['uuid' => $subscription->getSubscriptionUuid()]);
+    }
+  }
+
+  /**
+   * Queue an update check on all nodes.
+   */
+  public function queueAllNodesUpdate(): void {
+    $query = $this->entityTypeManager->getStorage('node')->getQuery();
+
+    $query->condition(BnfStateEnum::FIELD_NAME, BnfStateEnum::Imported->value);
+
+    $nids = $query->accessCheck(FALSE)->execute();
+    /** @var \Drupal\node\Entity\Node[] $nodes */
+    $nodes = $this->entityTypeManager->getStorage('node')->loadMultiple(array_values($nids));
+
+    $queue = $this->queueFactory->get('bnf_client_node_update');
+
+    foreach ($nodes as $node) {
+      $queue->createItem(['uuid' => $node->uuid()]);
+    }
+  }
+
+}
