2954c928765a907782c9b9680b83a329cc3a351d
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-03-31 16:26:04 +0200

    Do not force empty array for branches
    Since it breaks our Cypress test

--- web/modules/custom/dpl_library_agency/src/Branch/FbsBranchRepository.php
+++ web/modules/custom/dpl_library_agency/src/Branch/FbsBranchRepository.php
@@ -33,11 +33,7 @@ class FbsBranchRepository implements BranchRepositoryInterface {
   public function getBranches(): array {
     // Use an empty token if we don't have one. This may cause the API to throw
     // an exception, but that is acceptable.
-    if (!$token = $this->tokenHandler->getToken()) {
-      return [];
-    }
-
-    $api = $this->apiFactory->getAgencyApi($token->token ?? '');
+    $api = $this->apiFactory->getAgencyApi($this->tokenHandler->getToken()?->token ?? '');
     return array_map(function (AgencyBranch $branch) {
       return new Branch($branch->getBranchId(), $branch->getTitle());
     }, $api->getBranches());
