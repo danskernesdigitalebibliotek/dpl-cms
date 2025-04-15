5c31d75828c0e72b2bcb6b062d96471b9673fc40
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-04-15 14:00:17 +0200

    Simplify the BnfMapperManagerTest.

--- web/modules/custom/bnf/tests/src/Kernel/BnfMapperManagerTest.php
+++ web/modules/custom/bnf/tests/src/Kernel/BnfMapperManagerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\bnf\Kernel;
 
-use Drupal\bnf\GraphQL\Operations\GetNode\Node\CanonicalUrl\Link as CanonicalLink;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Changed\DateTime as ChangedDateTime;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Created\DateTime as CreatedDateTime;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\NodeArticle;
@@ -15,7 +14,6 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\Node;
-use Drupal\node\NodeInterface;
 use Drupal\paragraphs\Entity\Paragraph;
 
 /**
@@ -59,8 +57,6 @@ class BnfMapperManagerTest extends KernelTestBase {
       'uuid' => '982e0d87-f6b8-4b84-8de8-c8c8bcfef557',
     ])->willReturn($nodeProphecy);
 
-    $nodeProphecy->hasField('field_canonical_url')->willReturn(TRUE);
-
     $paragraphStorageProphecy->create([
       'type' => 'text_body',
     ])->willReturn($paragraphProphecy);
@@ -75,14 +71,6 @@ class BnfMapperManagerTest extends KernelTestBase {
       changed: ChangedDateTime::make(timestamp: 1735689661, timezone: 'UTC'),
       created: CreatedDateTime::make(timestamp: 1735689661, timezone: 'UTC'),
       publicationDate: PublicationDateDateTime::make(timestamp: 1735689661, timezone: 'UTC'),
-      canonicalUrl: CanonicalLink::make(
-        url: 'https://example.dk'
-      ),
-      overrideAuthor: 'this is an author',
-      showOverrideAuthor: TRUE,
-      subtitle: 'this is the subtitle',
-      teaserImage: NULL,
-      teaserText: 'this is a teaser text',
       paragraphs: [
         ParagraphTextBody::make(
           id: '982e0d87-f6b8-4b84-8de8-c8c8bcfef999',
@@ -96,23 +84,13 @@ class BnfMapperManagerTest extends KernelTestBase {
     $node = $mapper->map($graphqlNode);
 
     $this->assertSame($node, $nodeProphecy->reveal());
-    $nodeProphecy->set('title', 'Bibliotekarerne anbefaler læsning til den mørke tid')->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_canonical_url', [
-      'uri' => 'https://example.dk',
-    ])->shouldHaveBeenCalled();
     $nodeProphecy->set('field_paragraphs', [$paragraphProphecy->reveal()])->shouldHaveBeenCalled();
-    $nodeProphecy->set('status', NodeInterface::PUBLISHED)->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_publication_date', ["value" => "2025-01-01"])->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_subtitle', 'this is the subtitle')->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_override_author', 'this is an author')->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_show_override_author', TRUE)->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_teaser_text', 'this is a teaser text')->shouldHaveBeenCalled();
-    $nodeProphecy->set('field_teaser_image', [])->shouldHaveBeenCalled();
 
     $paragraphProphecy->set('field_body', [
       'value' => 'This is the text',
       'format' => 'with_format',
     ])->shouldHaveBeenCalled();
+
   }
 
 }
