718cc8f0f1dd310bf0e2ca6eda05d7fa01569c0a
Thomas Fini Hansen <xen@xen.dk>
2025-04-22 11:49:40 +0200

    Implement handling of internal links to existing content

--- web/modules/custom/bnf/src/Plugin/bnf_mapper/ParagraphGoLinkMapper.php
+++ web/modules/custom/bnf/src/Plugin/bnf_mapper/ParagraphGoLinkMapper.php
@@ -47,7 +47,23 @@ class ParagraphGoLinkMapper extends BnfMapperParagraphPluginBase {
     ]);
     $goLink->set('field_aria_label', $object->ariaLabel);
     $goLink->set('field_target_blank', $object->targetBlank);
-    $goLink->set('field_go_link', $this->getLinkValue($object->linkRequired));
+
+    if ($object->linkRequired->internal) {
+      /** @var \Drupal\node\Entity\Node[] $existing */
+      $existing = $this->entityTypeManager->getStorage('node')->loadByProperties(['uuid' => $object->linkRequired->id]);
+
+      if ($existing) {
+        $node = reset($existing);
+      }
+
+      $goLink->set('field_go_link', [
+        'uri' => $node->toUrl()->toString(),
+        'title' => $object->linkRequired->title,
+      ]);
+    }
+    else {
+      $goLink->set('field_go_link', $this->getLinkValue($object->linkRequired));
+    }
 
     return $goLink;
   }
--- web/modules/custom/bnf/tests/src/Unit/Mapper/ParagraphGoLinkMapperTest.php
+++ web/modules/custom/bnf/tests/src/Unit/Mapper/ParagraphGoLinkMapperTest.php
@@ -8,6 +8,9 @@ use Drupal\bnf\BnfMapperManager;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\GoLinkParagraph\LinkRequired\Link;
 use Drupal\bnf\GraphQL\Operations\GetNode\Node\Paragraphs\GoLinkParagraph\ParagraphGoLink;
 use Drupal\bnf\Plugin\bnf_mapper\ParagraphGoLinkMapper;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
 use Drupal\paragraphs\Entity\Paragraph;
 
 /**
@@ -69,6 +72,59 @@ class ParagraphGoLinkMapperTest extends EntityMapperTestBase {
     $this->entityProphecy->set('field_go_link', [
       'uri' => 'https://dr.dk/',
       'title' => 'DR.dk',
+    ])->shouldHaveBeenCalled();
+  }
+
+  /**
+   * Test internal mapping to existing nodes.
+   */
+  public function testInternalMappingToExistingContent(): void {
+    $this->storageProphecy->create([
+      'type' => 'go_link',
+    ])->willReturn($this->entityProphecy);
+
+    $manager = $this->prophesize(BnfMapperManager::class);
+
+    // Prophesize existing node.
+    $urlProphecy = $this->prophesize(Url::class);
+    $urlProphecy->toString()->willReturn('/our-url');
+    $nodeProphecy = $this->prophesize(Node::class);
+    $nodeProphecy->toUrl()->willReturn($urlProphecy);
+    $nodeStorage = $this->prophesize(EntityStorageInterface::class);
+    $nodeStorage->loadByProperties(['uuid' => 'content-uuid'])->willReturn([$nodeProphecy]);
+    $this->entityManagerProphecy->getStorage('node')->willReturn($nodeStorage);
+
+
+    $mapper = new ParagraphGoLinkMapper(
+      [],
+      '',
+      [],
+      $this->entityManagerProphecy->reveal(),
+      $manager->reveal(),
+    );
+
+    $graphqlElement = ParagraphGoLink::make(
+      id: 'paragraph-id',
+      ariaLabel: 'aria-label',
+      targetBlank: false,
+      linkRequired: Link::make(
+        internal: true,
+        title: 'Link',
+        url: '/someurl',
+        id: 'content-uuid',
+      ),
+    );
+
+    $paragraph = $mapper->map($graphqlElement);
+
+    $this->assertSame($paragraph, $this->entityProphecy->reveal());
+
+    $this->entityProphecy->set('field_aria_label', 'aria-label')->shouldHaveBeenCalled();
+    $this->entityProphecy->set('field_target_blank', FALSE)->shouldHaveBeenCalled();
+    $this->entityProphecy->set('field_target_blank', FALSE)->shouldHaveBeenCalled();
+    $this->entityProphecy->set('field_go_link', [
+      'uri' => '/our-url',
+      'title' => 'Link',
     ])->shouldHaveBeenCalled();
   }
 
