9abd5aca74485f2a73202a7750002f6e29fe89fd
Andreas Nielsen <andreas@reload.dk>
2025-04-25 16:12:59 +0200

    Added a new EventSubscriber called GoNodeRedirect.
    The EventSubscriber listens for requests that matches node entities.
    It checks if the request is an GO node entity, and redirects the user
    to the GO frontend if this is the case.

---
+++ web/modules/custom/dpl_go/src/EventSubscriber/GoNodeRedirect.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\dpl_go\EventSubscriber;
+
+use Drupal\Core\Url;
+use Drupal\dpl_go\Controller\GoController;
+use Drupal\node\NodeInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+/**
+ * Alter the response on GO nodes to a redirect to the external Go app.
+ *
+ * @package Drupal\dpl_go\EventSubscriber
+ */
+class GoNodeRedirect extends GoController implements EventSubscriberInterface {
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      'kernel.request' => [
+        ['redirectGoContent'],
+      ],
+    ];
+  }
+
+  /**
+   * Look up the content and redirect to the external Go app if it is a GO node.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The response event.
+   */
+  public function redirectGoContent(RequestEvent $event): void {
+    $request = $event->getRequest();
+
+    if ($request->attributes->get('_route') !== 'entity.node.canonical') {
+      return;
+    }
+
+    $node = $request->attributes->get('node');
+
+    if ($node instanceof NodeInterface && str_starts_with($node->bundle(), 'go_')) {
+      $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()])->toString();
+
+      $response = new RedirectResponse($this->getGoDomain() . $url);
+      $event->setResponse($response);
+    }
+  }
+
+}
