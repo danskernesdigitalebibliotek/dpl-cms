2e830a686b53bb3e779fa542ea6418ee37e5438e
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-07-03 12:42:30 +0200

    Export entity reference fields as simple UUIDs. DDFSAL-189
    As part of being able to import entity references, we want to easily get
    the UUIDs.
    If we do this the "GraphQL way", it will mean a bunch of entity unions,
    which quickly becomes very complicated.
    Instead, we'll create a simple GraphQL extension, that allows (certain)
    reference fields to be exported as UUIDs.

--- web/modules/custom/bnf/src/Plugin/GraphQL/SchemaExtension/BnfExtension.php
+++ web/modules/custom/bnf/src/Plugin/GraphQL/SchemaExtension/BnfExtension.php
@@ -36,8 +36,38 @@ class BnfExtension extends SdlSchemaExtensionPluginBase {
         ->map('entity', $builder->fromParent())
     ));
 
+    // Export entity reference fields, as simple UUIDs.
+    // This is not the "graphQL way" of doing things, but, it makes it much
+    // easier for us to recursively import linked content.
+    $referenceFields = [
+      [
+        'parent' => 'ParagraphNavGridManual',
+        'field_name' => 'field_content_references',
+        'output_field_name' => 'contentReferenceUuids',
+      ],
+      [
+        'parent' => 'ParagraphNavSpotsManual',
+        'field_name' => 'field_nav_spots_content',
+        'output_field_name' => 'navSpotsContentUuids',
+      ],
+      [
+        'parent' => 'ParagraphCardGridManual',
+        'field_name' => 'field_grid_content',
+        'output_field_name' => 'gridContentUuids',
+      ],
 
     ];
+
+    foreach ($referenceFields as $field) {
+      $registry->addFieldResolver($field['parent'], $field['output_field_name'],
+        $builder->callback(function ($paragraph) use ($field) {
+          $refs = $paragraph->get($field['field_name'])->referencedEntities();
+          return array_values(array_filter(array_map(function ($entity) {
+            return $entity->uuid();
+          }, $refs)));
+        })
+      );
+    }
 
     $thumbnailFields = ['MediaVideo', 'MediaVideotool'];
 
