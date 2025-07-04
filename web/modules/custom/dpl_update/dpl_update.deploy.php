b143c24a14aca30adf8498a6ab278eff6149bb45
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-07-04 12:10:33 +0200

    Testing a succeeding commit.
    Testing that the GH action should be green, when target code is available.

--- web/modules/custom/dpl_update/dpl_update.deploy.php
+++ web/modules/custom/dpl_update/dpl_update.deploy.php
@@ -403,3 +403,8 @@ function dpl_update_deploy_clean_medias(array &$sandbox): string {
 
   return "Archived {$sandbox['current']}/{$sandbox['total']} duplicate medias.";
 }
+
+function dpl_update_deploy_testing(): string {
+  _dpl_update_install_modules([]);
+  _dpl_update_alter_permissions([], []);
+}
