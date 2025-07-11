1592d71ed14ed837b14c7bd6abe56d5689b17e7a
Thomas Fini Hansen <xen@xen.dk>
2025-07-11 13:02:43 +0200

    Move FbiProfileType to Fbi

--- web/modules/custom/dpl_fbi/src/Fbi.php
+++ web/modules/custom/dpl_fbi/src/Fbi.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\dpl_fbi;
 
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\dpl_library_agency\GeneralSettings;
+use function Safe\preg_replace;
 
 /**
  * The FBI service.
@@ -12,9 +15,21 @@ use Drupal\dpl_library_agency\GeneralSettings;
 class Fbi {
 
   /**
+   * Configuration.
+   */
+  protected ImmutableConfig $config;
+
+  /**
+   * Constructor.
+   *
    * @todo Move the profile configuration to this module.
    */
-  public function __construct(protected GeneralSettings $agencySettings) {}
+  public function __construct(
+    protected GeneralSettings $agencySettings,
+    ConfigFactoryInterface $configFactory,
+  ) {
+    $this->config = $configFactory->get('dpl_fbi.settings');
+  }
 
   /**
    * Get profile names.
@@ -24,6 +39,31 @@ class Fbi {
    */
   public function getProfiles(): array {
     return $this->agencySettings->getFbiProfiles();
+  }
+
+  /**
+   * Get API URLs for FBI.
+   *
+   * @return array<string, string>
+   *   Service to URL mapping.
+   */
+  public function getServiceUrls(): array {
+    $baseUrl = $this->config->get('base_url');
+
+    $urls = [];
+    // Create an URL for each profile.
+    if ($baseUrl) {
+      foreach ($this->getProfiles() as $type => $profile) {
+        // The default FBI service has its own key with no suffix.
+        $service_key = $type === FbiProfileType::Default->value ? 'fbi' : sprintf('fbi-%s', $type);
+
+        // Create a service url with the profile embedded.
+        $base_url = preg_replace('/\[profile\]/', $profile, $baseUrl);
+        $urls[$service_key] = $base_url;
+      }
+    }
+
+    return $urls;
   }
 
 }
--- web/modules/custom/dpl_fbi/src/Hook/ReactAppsApiUrls.php
+++ web/modules/custom/dpl_fbi/src/Hook/ReactAppsApiUrls.php
@@ -4,31 +4,15 @@ declare(strict_types=1);
 
 namespace Drupal\dpl_fbi\Hook;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\dpl_fbi\Fbi;
-use Drupal\dpl_library_agency\FbiProfileType;
-use function Safe\preg_replace;
 
 /**
  * React apps API URLs.
  */
 class ReactAppsApiUrls {
 
-  /**
-   * Configuration.
-   */
-  protected ImmutableConfig $config;
-
-  public function __construct(
-    ConfigFactoryInterface $configFactory,
-    protected Fbi $fbi,
-  ) {
-    // @todo The profile settings belong in this module too, as does the
-    // FbiProfileType enum.
-    $this->config = $configFactory->get('dpl_fbi.settings');
-  }
+  public function __construct(protected Fbi $fbi) {}
 
   /**
    * API URLs for FBI.
@@ -38,25 +22,7 @@ class ReactAppsApiUrls {
    */
   #[Hook('dpl_react_apps_api_urls')]
   public function reactApiUrls(): array {
-    $baseUrl = $this->config->get('base_url');
-
-    $apiUrls = [];
-
-    // Create an URL for each profile.
-    if ($baseUrl) {
-      foreach ($this->fbi->getProfiles() as $type => $profile) {
-        $service_key = sprintf('fbi-%s', $type);
-        // The default FBI service has its own key with no suffix.
-        if ($type === FbiProfileType::DEFAULT->value) {
-          $service_key = 'fbi';
-        }
-        // Create a service url with the profile embedded.
-        $base_url = preg_replace('/\[profile\]/', $profile, $baseUrl);
-        $apiUrls[$service_key] = $base_url;
-      }
-    }
-
-    return $apiUrls;
+    return $this->fbi->getServiceUrls();
   }
 
 }
