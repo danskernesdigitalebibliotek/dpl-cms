3a3c82ab2dfefbbba8b8a2fc70f416ee69a9ae93
Thomas Fini Hansen <xen@xen.dk>
2025-08-05 14:47:47 +0200

    Add test of work title

---
+++ cypress/pages/work-page.ts
@@ -0,0 +1,14 @@
+import { PageObject, Elements } from '@hammzj/cypress-page-object';
+
+export class WorkPage extends PageObject {
+  public elements: Elements;
+
+  constructor() {
+    super({ path: '/work/:workId'});
+
+    this.addElements = {
+      page_title: () => cy.title(),
+    }
+  }
+
+}
