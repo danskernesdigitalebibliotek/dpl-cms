5194abe8c3c4538c35d15b09c256eac95438cbec
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-10-06 11:40:10 +0200

    Add cypress test for 'Copy from event-series to instance'. DDFSAL-254

--- cypress/e2e/events.cy.ts
+++ cypress/e2e/events.cy.ts
@@ -1,6 +1,7 @@
 import * as dayjs from 'dayjs';
 import 'cypress-if';
 import { typeInCkEditor } from '../helpers/helper-ckeditor';
+import { addParagraph } from '../helpers/helper-paragraph';
 
 const events = {
   singleEvent: {
@@ -9,6 +10,14 @@ const events = {
     recurType: 'Custom/Single Event',
     start: dayjs('2030-01-01T10:15:00'),
     end: dayjs('2030-01-01T16:15:00'),
+  },
+  repeatingEvent: {
+    title: 'Repeating event',
+    subtitle: 'A subtitle',
+    recurType: 'Weekly Event',
+    start: dayjs('2030-01-01'),
+    end: dayjs('2030-01-30'),
+    daysOfWeek: ['monday', 'thursday'],
   },
 };
 
@@ -95,6 +104,97 @@ describe('Events', () => {
 
     cy.contains(events.singleEvent.start.format('HH:mm')).should('not.exist');
     cy.contains('All day').should('be.visible');
+  });
+
+  it('copying all values from series to instance', () => {
+    cy.drupalLogin('/events/add/default');
+
+    // Ignore JS warnings, emitted from reccuring_events weekly input.
+    Cypress.on('uncaught:exception', () => {
+      return false;
+    });
+
+    cy.openParagraphsModal();
+
+    addParagraph('Text body');
+
+    cy.findByLabelText('Title').type(events.repeatingEvent.title);
+    cy.findByLabelText('Subtitle').type(events.repeatingEvent.subtitle);
+    cy.findByLabelText('Recur Type').select(events.repeatingEvent.recurType, {
+      // We have to use force when using Select2.
+      force: true,
+    });
+
+    cy.get(
+      '[data-drupal-selector="edit-weekly-recurring-date-0-end-value-date"]',
+    ).type(events.repeatingEvent.end.format('YYYY-MM-DD'));
+    cy.get(
+      '[data-drupal-selector="edit-weekly-recurring-date-0-value-date"]',
+    ).type(events.repeatingEvent.start.format('YYYY-MM-DD'));
+
+    events.repeatingEvent.daysOfWeek.forEach((day) => {
+      cy.get(`[name="weekly_recurring_date[0][days][${day}]"]`).check();
+    });
+
+    typeInCkEditor('Hello from series!');
+    cy.clickSaveButton();
+
+    // Editing a single instance.
+    cy.get('a[href^="/events/series"][href$="/edit"]').click({
+      // The admin toolbar gets in the way.
+      force: true,
+    });
+    cy.contains('Edit Instances').click();
+    cy.get(`a[aria-label="Edit ${events.repeatingEvent.title}"]`)
+      .first()
+      .click();
+
+    // Checking that the values don't already exist (inheritance default)
+    cy.get('[name="field_event_title[0][value]"]').should('be.empty');
+    cy.contains('Hello from series!').should('not.exist');
+
+    // Copy values from the series.
+    cy.contains('Copy values from series').click();
+    cy.contains('Copy values to instance').click();
+
+    // Checking that the values have been set, and add instance-only changes.
+    cy.get('[name="field_event_title[0][value]"]').should(
+      'contain.value',
+      events.repeatingEvent.title,
+    );
+    cy.get('[name="field_event_title[0][value]"]').type(' - instance');
+
+    cy.contains('Hello from series!').should('exist');
+    cy.get('[name="field_event_paragraphs_edit_all"]').click();
+    typeInCkEditor('Hello from instance!');
+    cy.clickSaveButton();
+
+    cy.go('back');
+
+    // Checking that copying from series *without* overwriting respects values.
+    cy.contains('Copy values from series').click();
+    cy.get('[data-drupal-selector="edit-overwrite-existing"]').uncheck();
+    cy.contains('Copy values to instance').click();
+
+    cy.contains('Hello from instance!').should('exist');
+    cy.contains('Hello from series!').should('not.exist');
+
+    cy.get('[name="field_event_title[0][value]"]').should(
+      'have.value',
+      `${events.repeatingEvent.title} - instance`,
+    );
+
+    // Re-copying and overwriting from series.
+    cy.contains('Copy values from series').click();
+    cy.get('[data-drupal-selector="edit-overwrite-existing"]').check();
+    cy.contains('Copy values to instance').click();
+
+    cy.get('[name="field_event_title[0][value]"]').should(
+      'not.contain.value',
+      `instance`,
+    );
+    cy.contains('Hello from instance!').should('not.exist');
+    cy.contains('Hello from series!').should('exist');
   });
 
   before(() => {
