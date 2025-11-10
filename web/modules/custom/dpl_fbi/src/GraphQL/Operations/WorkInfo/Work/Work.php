bf6bc7c5b84b67887ebb346c0b9e26738ec5b2d8
Thomas Fini Hansen <xen@xen.dk>
2025-11-10 14:02:16 +0100

    Add abstract and cover to work info call

--- web/modules/custom/dpl_fbi/src/GraphQL/Operations/WorkInfo/Work/Work.php
+++ web/modules/custom/dpl_fbi/src/GraphQL/Operations/WorkInfo/Work/Work.php
@@ -4,21 +4,34 @@ namespace Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work;
 
 /**
  * @property \Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work\Titles\WorkTitles $titles
+ * @property \Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work\Manifestations\Manifestations $manifestations
  * @property string $__typename
+ * @property array<int, string>|null $abstract
  */
 class Work extends \Spawnia\Sailor\ObjectLike
 {
     /**
      * @param \Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work\Titles\WorkTitles $titles
+     * @param \Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work\Manifestations\Manifestations $manifestations
+     * @param array<int, string>|null $abstract
      */
-    public static function make($titles): self
-    {
+    public static function make(
+        $titles,
+        $manifestations,
+        $abstract = 'Special default value that allows Sailor to differentiate between explicitly passing null and not passing a value at all.',
+    ): self {
         $instance = new self;
 
         if ($titles !== self::UNDEFINED) {
             $instance->titles = $titles;
         }
+        if ($manifestations !== self::UNDEFINED) {
+            $instance->manifestations = $manifestations;
+        }
         $instance->__typename = 'Work';
+        if ($abstract !== self::UNDEFINED) {
+            $instance->abstract = $abstract;
+        }
 
         return $instance;
     }
@@ -30,7 +43,9 @@ class Work extends \Spawnia\Sailor\ObjectLike
 
         return $converters ??= [
             'titles' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work\Titles\WorkTitles),
+            'manifestations' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Drupal\dpl_fbi\GraphQL\Operations\WorkInfo\Work\Manifestations\Manifestations),
             '__typename' => new \Spawnia\Sailor\Convert\NonNullConverter(new \Spawnia\Sailor\Convert\StringConverter),
+            'abstract' => new \Spawnia\Sailor\Convert\NullConverter(new \Spawnia\Sailor\Convert\ListConverter(new \Spawnia\Sailor\Convert\NonNullConverter(new \Spawnia\Sailor\Convert\StringConverter))),
         ];
     }
 
