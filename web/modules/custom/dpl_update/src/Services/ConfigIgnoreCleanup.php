f2080be9490e4246153c48e3270e7cfd5861e255
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-11-12 18:07:31 +0100

    Add maintainer service for ignored config. DDFSAL-469
    Add service methods that lets us look up items that are ignored as part
    of config_ignore_auto.
    This allows us to find problematic items, such as stuff that is
    unsupported by DPL.

---
+++ web/modules/custom/dpl_update/src/Services/ConfigIgnoreCleanup.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Drupal\dpl_update\Services;
+
+use Drupal\Core\Config\CachedStorage;
+use Drupal\Core\Config\Config;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\FileStorage;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use function Safe\preg_match;
+
+/**
+ * Maintainer service for keeping ignored config in check.
+ */
+class ConfigIgnoreCleanup {
+
+  /**
+   * The settings for config_ignore_auto.
+   */
+  public Config $configAutoIgnoreSettings;
+
+  /**
+   * The settings for config_ignore.
+   */
+  public Config $configIgnoreSettings;
+
+  /**
+   * Items that are ignored as part of config_ignore_auto.
+   *
+   * @var array<string>
+   */
+  public array $autoIgnoredItems;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function __construct(
+    protected ConfigFactoryInterface $configFactory,
+    // configSyncStorage looks up what exists as .yml files in the filesystem.
+    #[Autowire(service: 'config.storage.sync')]
+    protected FileStorage $configSyncStorage,
+    // configStorage looks up what exists as loaded config within the database.
+    #[Autowire(service: 'config.storage')]
+    protected CachedStorage $configStorage,
+    #[Autowire(service: 'logger.channel.dpl_update')]
+    protected LoggerInterface $logger,
+  ) {
+    $this->configAutoIgnoreSettings = $this->configFactory
+      ->getEditable('config_ignore_auto.settings');
+
+    $this->configIgnoreSettings = $this->configFactory
+      ->getEditable('config_ignore.settings');
+
+    $this->autoIgnoredItems = (array) $this->configAutoIgnoreSettings
+      ->get('ignored_config_entities');
+  }
+
+  /**
+   * Get the ignored items in config_ignore_auto that is not white-listed.
+   *
+   * We have a white-list of item patterns in config_ignore, for config
+   * that we expect any site (webmaster or not) to be able to override.
+   * Things such as dpl_* settings.
+   *
+   * This method looks through the ignored items, set by config_ignore_auto
+   * and finds any items that are NOT matching the allow-list patterns.
+   *
+   * @return array<string>
+   *   The ignored items.
+   */
+  public function getNonWhitelistedIgnores(): array {
+    // Getting the whitelists from config_ignore and config_ignore_auto.
+    $whitelist = $this->configIgnoreSettings->get('ignored_config_entities');
+    $whitelist = $whitelist + $this->configAutoIgnoreSettings->get('whitelist_config_entities');
+
+    return array_filter($this->autoIgnoredItems, function ($item) use ($whitelist) {
+      foreach ($whitelist as $pattern) {
+        // Convert shell-wildcard to regexp pattern.
+        $pattern = '/^' . strtr(preg_quote($pattern, '/'), ['\*' => '.*', '\?' => '.']) . '$/';
+        if (preg_match($pattern, $item)) {
+          // Item matches a whitelist pattern.
+          return FALSE;
+        }
+      }
+
+      // Item did NOT match any of the whitelist patterns.
+      return TRUE;
+    });
+  }
+
+  /**
+   * Finds any overriding config_ignore_auto items that are not whitelisted.
+   *
+   * These items can no longer be updated by DPL releases, and therefore
+   * is functionality no longer supported by DDF.
+   *
+   * This is displayed as a report on /admin/report/status and can also
+   * be pulled from drush using:
+   * drush php:eval "print_r(\Drupal::service('dpl_update.config_ignore_cleanup')->getUnsupportedIgnores());"
+   *
+   * @return array<string>
+   *   Unsupported items that ignore and override config from the filesystem.
+   */
+  public function getUnsupportedIgnores(): array {
+    $items = $this->getNonWhitelistedIgnores();
+    $unsupported_items = [];
+
+    // Loop through the non-whitelisted ignores, and check if it exists as part
+    // of the DPL codebase.
+    foreach ($items as $item) {
+      if (!empty($this->configSyncStorage->read($item))) {
+        $unsupported_items[] = $item;
+      }
+    }
+
+    return $unsupported_items;
+  }
+
+  /**
+   * Find auto-ignores that do not differ from codebase.
+   *
+   * @return array<string>
+   *   The ignored items.
+   */
+  public function getUnusedAutoIgnores(): array {
+    $items = $this->autoIgnoredItems;
+    if (empty($items)) {
+      return [];
+    }
+
+    $unused_items = [];
+
+    foreach ($items as $item) {
+      if ($this->configSyncStorage->read($item) === $this->configStorage->read($item)) {
+        $unused_items[] = $item;
+      }
+    }
+
+    return $unused_items;
+  }
+
