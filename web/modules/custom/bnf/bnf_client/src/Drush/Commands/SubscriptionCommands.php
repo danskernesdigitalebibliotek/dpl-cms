18a6f1f7c320a2bf3ebae72db012992c56d38696
Kasper Birch <kasperbirch1@gmail.com>
2026-01-02 11:42:36 +0100

    Add tag option to Drush subscription create command
    Expose the tagName parameter through a --tag option, allowing CLI
    users to create subscriptions with automatic tagging. Also adds
    proper Argument attribute for label and updated usage examples.

--- web/modules/custom/bnf/bnf_client/src/Drush/Commands/SubscriptionCommands.php
+++ web/modules/custom/bnf/bnf_client/src/Drush/Commands/SubscriptionCommands.php
@@ -12,6 +12,7 @@ use Drush\Attributes\Argument;
 use Drush\Attributes\Command;
 use Drush\Attributes\FieldLabels;
 use Drush\Attributes\Help;
+use Drush\Attributes\Option;
 use Drush\Attributes\Usage;
 use Drush\Commands\AutowireTrait;
 use Drush\Commands\DrushCommands;
@@ -46,12 +47,18 @@ class SubscriptionCommands extends DrushCommands {
   #[Command(name: 'bnf:subscription:create')]
   #[Help(description: 'Create a new subscription')]
   #[Argument(name: 'uuid', description: 'UUID to subscribe')]
+  #[Argument(name: 'label', description: 'Label for the subscription')]
+  #[Option(name: 'tag', description: 'Tag name to create and associate with the subscription')]
   #[Usage(
-    name: 'drush bnf:subscription:create 8f647000-cb67-40d0-b942-3f7fbf899c88',
+    name: 'drush bnf:subscription:create 8f647000-cb67-40d0-b942-3f7fbf899c88 "My subscription"',
     description: 'Subscribe to 8f647000-cb67-40d0-b942-3f7fbf899c88.'
   )]
-  public function createSubscription(string $uuid = '', string $label = ''): void {
-    $feedback = $this->subscriptionCreator->addSubscription($uuid, $label);
+  #[Usage(
+    name: 'drush bnf:subscription:create 8f647000-cb67-40d0-b942-3f7fbf899c88 "My subscription" --tag="My Tag"',
+    description: 'Subscribe with automatic tagging of imported content.'
+  )]
+  public function createSubscription(string $uuid = '', string $label = '', array $options = ['tag' => NULL]): void {
+    $feedback = $this->subscriptionCreator->addSubscription($uuid, $label, $options['tag']);
     $this->output()->writeln($feedback);
   }
 
